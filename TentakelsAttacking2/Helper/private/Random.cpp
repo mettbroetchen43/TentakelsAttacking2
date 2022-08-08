8a2f23ed56fb313e9e29a351a8c2bc9fe7c25b15
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-08-08 20:29:50 +0200

    added random

---
+++ TentakelsAttacking2/Helper/private/Random.cpp
@@ -0,0 +1,38 @@
+//
+// Purpur Tentakel
+// original by coder2k (https://gist.github.com/mgerhold/353e39da27ae9b22c614bc264c8a3d18)
+// 08.08.2022
+//
+
+#include "Random.h"
+#include <chrono>
+
+Random& Random::GetInstance() {
+	static Random random;
+	return random;
+}
+
+Random::Random() {
+	randomize();
+}
+
+auto Random::random(std::mt19937_64::result_type upperBoundExclusive) -> std::mt19937_64::result_type {
+	return mUniformIntDistribution(mGenerator) % upperBoundExclusive;
+}
+
+auto Random::random() -> float {
+	return mUniformRealDistribution(mGenerator);
+}
+
+auto Random::setRandomSeed(std::mt19937_64::result_type seed) -> void {
+	mGenerator.seed(seed);
+	mSeed = seed;
+}
+
+auto Random::getRandomSeed() const -> std::mt19937_64::result_type {
+	return mSeed;
+}
+
+auto Random::randomize() -> void {
+	setRandomSeed(std::chrono::system_clock::now().time_since_epoch().count());
+}
---
+++ TentakelsAttacking2/Helper/public/Random.h
@@ -0,0 +1,27 @@
+//
+// Purpur Tentakel
+// original by coder2k (https://gist.github.com/mgerhold/353e39da27ae9b22c614bc264c8a3d18)
+// 08.08.2022
+//
+
+#pragma once
+#include <random>
+
+class Random {
+public:
+	static Random& GetInstance();
+
+	[[nodiscard]] auto random(std::mt19937_64::result_type upperBoundExclusive)->std::mt19937_64::result_type;
+	[[nodiscard]] auto random() -> float;
+	auto setRandomSeed(std::mt19937_64::result_type seed) -> void;
+	[[nodiscard]] auto getRandomSeed() const->std::mt19937_64::result_type;
+	auto randomize() -> void;
+
+private:
+	Random();
+	Random(const Random& old) = delete;
+	std::mt19937_64 mGenerator;
+	std::mt19937_64::result_type mSeed;
+	std::uniform_real_distribution<float> mUniformRealDistribution;
+	std::uniform_int_distribution<std::mt19937_64::result_type> mUniformIntDistribution;
+};
