8cbbd64f4100df4a72e0d4354f0cf3e7ceb9746d
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-09-10 18:50:59 +0200

    renamed FocusEvents implemented PopUpEvents

--- TentakelsAttacking2/Events/public/Events.h
+++ TentakelsAttacking2/Events/public/Events.h
@@ -6,6 +6,7 @@
 #pragma once
 #include "MainEvent.h"
 #include "SoundType.h"
+#include <string>
 
 class Focusable;
 
@@ -44,4 +45,33 @@ public:
 class SelectFocusElementEvent : public FocusEvent {
 public:
 	using FocusEvent::FocusEvent;
+};
+class NewFocusLayerEvent :public FocusEvent {
+public:
+	using FocusEvent::FocusEvent;
+};
+class DeleteFocusLayerEvent : public FocusEvent {
+public:
+	using FocusEvent::FocusEvent;
+};
+
+class PopUpEvent : public Event {
+private:
+	std::string m_title;
+	std::string m_subTitle;
+
+public:
+	PopUpEvent(std::string const& title, std::string const& subTile)
+		:m_title(title), m_subTitle(subTile) {}
+
+	[[nodiscard]] std::string const& GetTitle() const {
+		return m_title;
+	}
+	[[nodiscard]] std::string const& GetSubTitle() const {
+		return m_subTitle;
+	}
+};
+class ShowMessagePopUpEvent : public PopUpEvent {
+public:
+	using PopUpEvent::PopUpEvent;
 };
