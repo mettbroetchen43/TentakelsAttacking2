c9ff85b8f40dfe77e3492e0f2a99526efb462a9c
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-10-04 14:22:54 +0200

    implemented RemoveListener

--- TentakelsAttacking2/Events/public/EventManager.h
+++ TentakelsAttacking2/Events/public/EventManager.h
@@ -12,6 +12,13 @@ public:
 	void AddListener(EventListener* eventListener) {
 		m_listeners.push_back(eventListener);
 	}
+
+	void RemoveListener(EventListener* eventlistener) {
+		m_listeners.erase(std::remove(
+			m_listeners.begin(), m_listeners.end(), eventlistener),
+			m_listeners.end());
+	}
+
 	void InvokeEvent(Event const& event) const {
 		for (auto const listener : m_listeners) {
 			listener->OnEvent(event);
