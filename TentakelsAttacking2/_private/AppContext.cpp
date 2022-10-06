8edcf8cc442593831090070de81fba9c2756f1cf
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-10-06 17:30:17 +0200

    added maxPlayerCount

--- TentakelsAttacking2/_private/AppContext.cpp
+++ TentakelsAttacking2/_private/AppContext.cpp
@@ -16,7 +16,9 @@ std::string const& AppContext::Version() const {
 std::string const& AppContext::CopyRight() const {
 	return m_copyRight;
 }
-
+size_t AppContext::MaxPlayerCount() const {
+	return m_maxPlayerCount;
+}
 
 AppContext::AppContext() {
 	eventManager.AddListener(&soundManager);
--- TentakelsAttacking2/_public/AppContext.h
+++ TentakelsAttacking2/_public/AppContext.h
@@ -17,9 +17,11 @@ struct AppContext {
 	[[nodiscard]] static AppContext& GetInstance();
 	[[nodiscard]] std::string const& Version() const;
 	[[nodiscard]] std::string const& CopyRight() const;
+	[[nodiscard]] size_t MaxPlayerCount() const;
 
 private:
 	AppContext();
 	std::string m_version = "v0.0.0";
 	std::string m_copyRight = "(c) Purpur Tentakel";
+	size_t m_maxPlayerCount = 7;
 };
