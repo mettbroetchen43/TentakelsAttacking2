1f808d51274d31dc593d5647e139a83578d43e0c
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-10-12 00:52:28 +0200

    Added EventsListener for GameManager

--- TentakelsAttacking2/Game/private/GameManager.cpp
+++ TentakelsAttacking2/Game/private/GameManager.cpp
@@ -4,6 +4,41 @@
 //
 
 #include "GameManager.h"
+#include "AppContext.h"
+
+bool GameManager::ValidAddPlayer() const {
+	return AppContext::GetInstance().playerCollection.MaxPlayerCount()
+		> m_players.size();
+}
+
+GameManager::GameManager() {
+	AppContext::GetInstance().eventManager.AddListener(this);
+}
+
+void GameManager::AddPlayer(AddPlayerEvent const* event) {
+	if (!ValidAddPlayer()) {
+		auto UIEvent = ShowMessagePopUpEvent(
+			"Max Player Count",
+			"No more Player slots are available"
+		);
+		AppContext::GetInstance().eventManager.InvokeEvent(UIEvent);
+		return;
+	}
+
+	unsigned int newID = m_players.size() + 1;
+	auto player = std::make_shared<Player>(
+		newID,
+		PlayerType::HUMAN
+		);
+	m_players.push_back(player);
+
+	auto AddEvent = AddPlayerUIEvent(
+		newID,
+		event->GetName(),
+		event->GetColor()
+	);
+	AppContext::GetInstance().eventManager.InvokeEvent(AddEvent);
+}
 
 std::vector<std::shared_ptr<Player>>& GameManager::GetPlayers() {
 	return m_players;
@@ -16,4 +51,12 @@ void GameManager::Update() {
 	m_galaxy.PreUpdate(); // bevor main update -> z.B. move
 	m_galaxy.Update();  // main update
 	m_galaxy.PostUpdate(); // debug Print
+}
+
+void GameManager::OnEvent(Event const& event) {
+
+	if (auto const* playerEvent = dynamic_cast<AddPlayerEvent const*>(&event)) {
+		AddPlayer(playerEvent);
+		return;
+	}
 }
--- TentakelsAttacking2/Game/public/GameManager.h
+++ TentakelsAttacking2/Game/public/GameManager.h
@@ -6,16 +6,28 @@
 #pragma once
 #include "Player.h"
 #include "Galaxy.h"
+#include "EventListener.hpp"
+#include "UIEvents.hpp"
+#include "GenerelEvents.hpp"
 #include <vector>
 
-class GameManager {
+class GameManager : public EventListener {
 private:
 	std::vector<std::shared_ptr<Player>> m_players;
 	Galaxy m_galaxy;
+
+	[[nodiscard]] bool ValidAddPlayer() const;
+
 public:
+	GameManager();
+
+	void AddPlayer(AddPlayerEvent const* event);
+
 	[[nodiscard]] std::vector<std::shared_ptr<Player>>& GetPlayers();
 	[[nodiscard]] std::vector<std::shared_ptr<Player>> const& GetPlayers() const;
 
 	void Update();
+
+	void OnEvent(Event const& event) override;
 };
 
