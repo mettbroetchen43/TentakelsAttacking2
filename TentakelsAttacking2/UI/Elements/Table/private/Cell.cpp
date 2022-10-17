a0bcebc99192b74a6a666d53d2a5f5a7e9ccd222
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-10-17 23:01:55 +0200

    deleted old files

--- TentakelsAttacking2/UI/Elements/Table/private/Cell.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Purpur Tentakel
-// 29.09.22
-//
-
-#include "Cell.h"
-#include "AppContext.h"
-
--- TentakelsAttacking2/UI/Elements/Table/private/StringCell.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Purpur Tentakel
-// 01.10.2022
-//
-
-#include "StringCell.h"
-#include "AppContext.h"
-#include "HTextProcessing.h"
-#include "Table.h"
