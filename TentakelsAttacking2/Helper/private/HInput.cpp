256ba0507d978944b93d810917eb8577de3d7a08
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-10-24 19:13:05 +0200

    implemented more helper

--- TentakelsAttacking2/Helper/private/HInput.cpp
+++ TentakelsAttacking2/Helper/private/HInput.cpp
@@ -6,14 +6,40 @@
 #include "HInput.h"
 #include <raylib.h>
 
-bool IsConfirmInput() {
+bool IsConfirmInputPressed() {
     return IsKeyPressed(KEY_ENTER)
         or IsKeyPressed(KEY_KP_ENTER)
         or IsKeyPressed(KEY_SPACE);
 }
-bool IsBackInput() {
+bool IsConfirmInputReleased() {
+    return IsKeyReleased(KEY_ENTER)
+        or IsKeyReleased(KEY_KP_ENTER)
+        or IsKeyReleased(KEY_SPACE);
+}
+bool IsConfirmInputDown() {
+    return IsKeyDown(KEY_ENTER)
+        or IsKeyDown(KEY_KP_ENTER)
+        or IsKeyDown(KEY_SPACE);
+}
+bool IsConfirmInputUp() {
+    return IsKeyUp(KEY_ENTER)
+        or IsKeyUp(KEY_KP_ENTER)
+        or IsKeyUp(KEY_SPACE);
+}
+
+bool IsBackInputPressed() {
     return IsKeyPressed(KEY_ESCAPE);
 }
+bool IsBackInputReleased() {
+    return IsKeyReleased(KEY_ESCAPE);
+}
+bool IsBackInputDown() {
+    return IsKeyDown(KEY_ESCAPE);
+}
+bool IsBackInputUp() {
+    return IsKeyUp(KEY_ESCAPE);
+}
+
 bool IsQuitInput() {
     return IsKeyDown(KEY_LEFT_ALT)
         and IsKeyDown(KEY_F4);
--- TentakelsAttacking2/Helper/public/HInput.h
+++ TentakelsAttacking2/Helper/public/HInput.h
@@ -5,7 +5,15 @@
 
 #pragma once
 
-[[nodiscard]] bool IsConfirmInput();
-[[nodiscard]] bool IsBackInput();
+[[nodiscard]] bool IsConfirmInputPressed();
+[[nodiscard]] bool IsConfirmInputReleased();
+[[nodiscard]] bool IsConfirmInputDown();
+[[nodiscard]] bool IsConfirmInputUp();
+
+[[nodiscard]] bool IsBackInputPressed();
+[[nodiscard]] bool IsBackInputReleased();
+[[nodiscard]] bool IsBackInputDown();
+[[nodiscard]] bool IsBackInputUp();
+
 [[nodiscard]] bool IsQuitInput();
 
