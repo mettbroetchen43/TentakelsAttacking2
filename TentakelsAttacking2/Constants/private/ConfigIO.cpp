a9203f836a24fcca1b7f0908d3dbae90b6d464b7
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-10-28 13:28:16 +0200

    saved Worl

--- TentakelsAttacking2/Constants/private/ConfigIO.cpp
+++ TentakelsAttacking2/Constants/private/ConfigIO.cpp
@@ -24,6 +24,8 @@ void SaveConfig() {
 		std::cout << "Cant Open Config\n";
 		return;
 	}
+	std::string toSave = "//\n// Purpur Tentakel\n// Tentakels Attacking\n// Config\n//\n";
+
 
 	auto headline = [](std::string const& headline, std::string& toSave) {
 		toSave += "//\n// " + headline + '\n';
@@ -31,8 +33,7 @@ void SaveConfig() {
 	auto entry = [](std::string const& entry, std::string const& message, std::string& toSave) {
 		toSave += entry + " - " + message + "\n";
 	};
-	std::string toSave = "//\n// Purpur Tentakel\n// Tentakels Attacking\n// Config\n//\n";
-
+	
 	headline("Globals", toSave);
 	entry(std::to_string(constants.global.startingModeFullScreen), "Starting Full Screen", toSave);
 
@@ -40,6 +41,26 @@ void SaveConfig() {
 	entry(std::to_string(constants.player.minPlayerCount), "Min Player Count", toSave);
 	entry(std::to_string(constants.player.maxPlayerCount), "Max Player Count", toSave);
 
+	headline("World", toSave);
+	entry(std::to_string(constants.world.minPlanetCount), "Min Planet Count", toSave);
+	entry(std::to_string(constants.world.maxPlanetCount), "Max Planet Count", toSave);
+
+	entry(std::to_string(constants.world.minDiemnsionX), "Min Dimension X", toSave);
+	entry(std::to_string(constants.world.maxDiemnsionX), "Max Dimension X", toSave);
+	entry(std::to_string(constants.world.minDiemnsionY), "Min Dimension Y", toSave);
+	entry(std::to_string(constants.world.maxDiemnsionY), "Max Dimension Y", toSave);
+
+	headline("Sound", toSave);
+	
 	file << toSave;
 	file.close();
 }
+/*
+size_t minPlanetCount = 20;
+size_t maxPlanetCount = 100;
+
+size_t minDiemnsionX = 40;
+size_t minDiemnsionY = 20;
+size_t maxDiemnsionX = 200;
+size_t maxDiemnsionY = 160;
+*/
--- TentakelsAttacking2/Saves/config.tac
+++ TentakelsAttacking2/Saves/config.tac
@@ -10,3 +10,11 @@
 // Player
 2 - Min Player Count
 7 - Max Player Count
+//
+// World
+20 - Min Planet Count
+100 - Max Planet Count
+40 - Min Dimension X
+200 - Max Dimension X
+20 - Min Dimension Y
+160 - Max Dimension Y
