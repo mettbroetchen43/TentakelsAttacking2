ac24f37234249f4b5948e61d46925b733a577659
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-10-31 19:22:27 +0100

    current constants

--- TentakelsAttacking2/Constants/public/CWorld.hpp
+++ TentakelsAttacking2/Constants/public/CWorld.hpp
@@ -7,10 +7,14 @@
 
 struct CWorld {
 	size_t minPlanetCount = 20;
+	size_t currentPlanetCount = 50;
 	size_t maxPlanetCount = 100;
 
 	size_t minDiemnsionX = 40;
-	size_t minDiemnsionY = 20;
+	size_t currentDimensionX = 100;
 	size_t maxDiemnsionX = 200;
+
+	size_t minDiemnsionY = 20;
+	size_t currentDimensionY = 80;
 	size_t maxDiemnsionY = 160;
 };
