597385fa0fdd453d12b46cb008a486b7be43b8a9
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-10 22:50:24 +0100

    remove UIPlanets // add IsScaling

--- TentakelsAttacking2/UI/Scene/SupportScenes/private/GalaxyAndSlider.cpp
+++ TentakelsAttacking2/UI/Scene/SupportScenes/private/GalaxyAndSlider.cpp
@@ -12,7 +12,7 @@
 #include "ClassicButton.h"
 #include <iostream>
 
-void GalaxyScene::Initialize() {
+void GalaxyScene::Initialize(Vector2 resolution) {
 	AppContext& appContext = AppContext::GetInstance();
 
 	// slider
@@ -21,8 +21,8 @@ void GalaxyScene::Initialize() {
 		GetElementSize(0.03f, 0.5f),
 		Alignment::MID_LEFT,
 		false,
-		1.7f,
-		m_resolution
+		m_galaxayScaleFacor,
+		resolution
 		);
 	m_verticalSlider->SetScrolling(true);
 
@@ -31,8 +31,8 @@ void GalaxyScene::Initialize() {
 		GetElementSize(0.5f, 0.05f),
 		Alignment::BOTTOM_MID,
 		true,
-		10.0f,
-		m_resolution
+		m_galaxayScaleFacor,
+		resolution
 		);
 	m_horisontalSlider->SetScrolling(true);
 
@@ -42,117 +42,67 @@ void GalaxyScene::Initialize() {
 		GetElementPosition(1.0f, 1.0f),
 		GetElementSize(0.05f, 0.05f),
 		Alignment::BOTTOM_RIGHT,
-		m_resolution,
+		resolution,
 		"+",
 		SoundType::CLICKED_RELEASE_STD
 		);
 	m_zoomInBtn->SetOnPress([this]() {
 		this->SetScale(true);
 		});
-	m_elements.push_back(m_zoomInBtn);
 
 	m_zoomOutBtn = std::make_shared<ClassicButton>(
 		101,
 		GetElementPosition(0.95f, 1.0f),
 		GetElementSize(0.05f, 0.05f),
 		Alignment::BOTTOM_RIGHT,
-		m_resolution,
+		resolution,
 		"-",
 		SoundType::CLICKED_RELEASE_STD
 		);
 	m_zoomOutBtn->SetOnPress([this]() {
 		this->SetScale(false);
 		});
-	m_elements.push_back(m_zoomOutBtn);
 
 	// Galaxy
 	auto event = GetGalaxyCopy();
 	appContext.eventManager.InvokeEvent(event);
 }
 
-void GalaxyScene::InitialGalaxy() {
-	AppContext& appContext = AppContext::GetInstance();
-	m_scaleFacor = 1.0f;
-
-	auto galaxyPos = GetElementPosition(0.035f, 0.0f);
-	auto galaxySize = GetElementSize(0.965f, 0.94f);
-	m_galaxyColiderDraw = {
-		galaxyPos.x * m_resolution.x,
-		galaxyPos.y * m_resolution.y,
-		galaxySize.x * m_resolution.x,
-		galaxySize.y * m_resolution.y
-	};
-
-	float offsetX = 50;
-	float offsetY = 35;
-
-	m_galaxyColider = {
-		m_galaxyColiderDraw.x - offsetX,
-		m_galaxyColiderDraw.y - offsetY,
-		m_galaxyColiderDraw.width - 2 * offsetX,
-		m_galaxyColiderDraw.height - 2 * offsetY,
-	};
-
-	float scalingFactorX = m_galaxyColider.width / m_currentGalaxy->GetSize().x;
-	float scalingFactorY = m_galaxyColider.height / m_currentGalaxy->GetSize().y;
-	float dummyScaling = scalingFactorX > scalingFactorY ? scalingFactorY : scalingFactorX;
-
-	for (auto const& p : m_currentGalaxy->GetPlanets()) {
-
-		auto color = appContext.playerCollection.GetColorByID(p->GetID());
-
-		auto position = p->GetPos();
-		auto planet = std::make_shared<PlanetUI>(
-			GetElementPosition(
-				position.x * dummyScaling / m_colider.width
-					+ 0.035f * m_size.x
-					+ offsetX / m_colider.width,
-				position.y * dummyScaling / m_colider.height
-					+ offsetY / m_colider.height
-			),
-			p->GetID(),
-			color,
-			m_resolution
-			);
-
-		m_galaxyElements.push_back(planet);
-	}
-}
-void GalaxyScene::UpdateGalaxy() {
-	AppContext& appContext = AppContext::GetInstance();
-
-}
-
 void GalaxyScene::SetScale(bool scaleIn) {
-	if (scaleIn) { m_scaleFacor += 0.01f; }
-	else { m_scaleFacor -= 0.01f; }
+	if (scaleIn) { m_galaxayScaleFacor += 0.01f; }
+	else { m_galaxayScaleFacor -= 0.01f; }
 
-	if (m_scaleFacor < 1.0f) { m_scaleFacor = 1.0f; }
+	if (m_galaxayScaleFacor < 1.0f) { m_galaxayScaleFacor = 1.0f; }
 
-	m_horisontalSlider->SetAboluteDimension(m_scaleFacor);
-	m_verticalSlider->SetAboluteDimension(m_scaleFacor);
-
-	std::cout << m_scaleFacor << '\n';
+	m_horisontalSlider->SetAboluteDimension(m_galaxayScaleFacor);
+	m_verticalSlider->SetAboluteDimension(m_galaxayScaleFacor);
 }
 
 GalaxyScene::GalaxyScene(Vector2 pos, Vector2 size, Alignment alignment,
 	Vector2 resolution)
-	: Scene(pos, size, alignment), m_resolution(resolution) {
-	m_colider = GetAlignedCollider(m_pos, m_size, alignment, m_resolution);
+	: Scene(pos, size, alignment) {
+	GetAlignedCollider(m_pos, m_size, alignment, resolution);
 
 	AppContext::GetInstance().eventManager.AddListener(this);
 
-	Initialize();
+	Initialize(resolution);
 }
 GalaxyScene::~GalaxyScene() {
 	AppContext::GetInstance().eventManager.RemoveListener(this);
 }
 
+void GalaxyScene::SetIsScaling(bool isScaling) {
+	m_isScaling = isScaling;
+}
+bool GalaxyScene::GetIsScaling() const {
+	return m_isScaling;
+}
+
 void GalaxyScene::OnEvent(Event const& event) {
 
 	if (auto const* GalaxyEvent = dynamic_cast<SendGalaxyCopy const*>(&event)) {
-		m_currentGalaxy = GalaxyEvent->GetGalaxy();
-		InitialGalaxy();
+		/*m_currentGalaxy = GalaxyEvent->GetGalaxy();
+		InitialGalaxy();*/
 		return;
 	}
 }
@@ -160,38 +110,42 @@ void GalaxyScene::OnEvent(Event const& event) {
 void GalaxyScene::CheckAndUpdate(Vector2 const& mousePosition, AppContext const& appContext) {
 	Scene::CheckAndUpdate(mousePosition, appContext);
 
-	if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)) {
-		float mouse = GetMouseWheelMove();
-		if (mouse != 0.0f) {
-			SetScale(mouse > 0);
-			SetScale(mouse > 0);
+	if (m_isScaling) {
+		m_zoomInBtn->CheckAndUpdate(mousePosition, appContext);
+		m_zoomOutBtn->CheckAndUpdate(mousePosition, appContext);
+
+		if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)) {
+			float mouse = GetMouseWheelMove();
+			if (mouse != 0.0f) {
+				SetScale(mouse > 0);
+				SetScale(mouse > 0);
+			}
 		}
 	}
 
-	if (m_scaleFacor > 1.0f) {
+	if (m_galaxayScaleFacor > 1.0f) {
 		m_horisontalSlider->CheckAndUpdate(mousePosition, appContext);
 		m_verticalSlider->CheckAndUpdate(mousePosition, appContext);
-	}
-
-	for (auto& e : m_galaxyElements) {
-		e->CheckAndUpdate(mousePosition, appContext);
 	}
 }
 void GalaxyScene::Render(AppContext const& appContext) {
 	Scene::Render(appContext);
 
-	for (auto& e : m_galaxyElements) {
-		e->Render(appContext);
-	}
-
-	if (m_scaleFacor > 1.0f) {
+	if (m_galaxayScaleFacor > 1.0f) {
 		m_horisontalSlider->Render(appContext);
 		m_verticalSlider->Render(appContext);
 	}
 
-	/*DrawRectangleLinesEx(
-		m_galaxyColiderDraw,
-		3.0f,
-		WHITE
-	);*/
+	if (m_isScaling) {
+		m_zoomInBtn->Render(appContext);
+		m_zoomOutBtn->Render(appContext);
+	}
+}
+void GalaxyScene::Resize(Vector2 resolution, AppContext const& appContext) {
+	Scene::Resize(resolution, appContext);
+
+	m_verticalSlider->Resize(resolution, appContext);
+	m_horisontalSlider->Resize(resolution, appContext);
+	m_zoomInBtn->Resize(resolution, appContext);
+	m_zoomOutBtn->Resize(resolution, appContext);
 }
--- TentakelsAttacking2/UI/Scene/SupportScenes/public/GalaxyAndSlider.h
+++ TentakelsAttacking2/UI/Scene/SupportScenes/public/GalaxyAndSlider.h
@@ -14,22 +14,14 @@ class ClassicButton;
 
 class GalaxyScene final : public Scene, public EventListener {
 private:
-	float m_scaleFacor = 1.0f;
-	Vector2 m_resolution;
-	Galaxy const* m_currentGalaxy;
-	Rectangle m_galaxyColiderDraw;
-	Rectangle m_galaxyColider;
-	Rectangle m_colider;
-	std::vector<std::shared_ptr<UIElement>> m_galaxyElements;
+	bool m_isScaling = true;
+	float m_galaxayScaleFacor = 1.0f;
 	std::shared_ptr<Slider> m_verticalSlider;
 	std::shared_ptr<Slider> m_horisontalSlider;
 	std::shared_ptr<ClassicButton> m_zoomInBtn;
 	std::shared_ptr<ClassicButton> m_zoomOutBtn;
 
-	void Initialize();
-
-	void InitialGalaxy();
-	void UpdateGalaxy();
+	void Initialize(Vector2 resolution);
 
 	void SetScale(bool ScaleIn);
 
@@ -37,8 +29,12 @@ public:
 	GalaxyScene(Vector2 pos, Vector2 size, Alignment alignment, Vector2 resolution);
 	~GalaxyScene();
 
+	void SetIsScaling(bool isScaling);
+	[[nodiscard]] bool GetIsScaling() const;
+
 	void OnEvent(Event const& event) override;
 
 	void Render(AppContext const& appContext) override;
 	void CheckAndUpdate(Vector2 const& mousePosition, AppContext const& appContext) override;
+	void Resize(Vector2 resolution, AppContext const& appContext) override;
 };
