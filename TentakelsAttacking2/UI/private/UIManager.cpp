2fba8e68c94140bdb35c1878bdefb2f7946efe9c
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-13 21:57:34 +0100

    removed duplicate initialization

--- TentakelsAttacking2/UI/private/UIManager.cpp
+++ TentakelsAttacking2/UI/private/UIManager.cpp
@@ -81,7 +81,7 @@ void UIManager::UILoop() {
 }
 
 UIManager::UIManager()
-	: m_appContext(AppContext::GetInstance()), m_resolution({ 0.0f,0.0f }), m_sceneManager(this),m_closeWindow(false) {
+	: m_appContext(AppContext::GetInstance()), m_resolution({ 0.0f,0.0f }), m_sceneManager(this) {
 	SetTargetFPS(60);
 	SetExitKey(KeyboardKey::KEY_NULL);
 
