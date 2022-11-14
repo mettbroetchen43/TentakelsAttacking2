720e3dd9b61d794b1067df65ffdba44ed5afb12f
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-15 00:28:27 +0100

    slider -> need to chance color

--- TentakelsAttacking2/UI/Elements/PopUp/private/InitialSoundLevelPopUp.cpp
+++ TentakelsAttacking2/UI/Elements/PopUp/private/InitialSoundLevelPopUp.cpp
@@ -4,8 +4,40 @@
 //
 
 #include "InitialSoundLevelPopUp.h"
+#include "ClassicButton.h"
+#include "SliderAndInputLine.h"
+#include "CheckBox.h"
 
-void InitialSoundLevelPopUp::Initialize(Vector2 resolution) {}
+void InitialSoundLevelPopUp::Initialize(Vector2 resolution) {
+	AppContext& appContext = AppContext::GetInstance();
+	
+	m_slider = std::make_shared<SliderAndInputLine>(
+		2, // 3
+		GetElementPosition(m_pos, m_size, 0.5f, 0.75f),
+		GetElementSize(m_size, 0.7f, 0.1f),
+		Alignment::BOTTOM_MID,
+		0,
+		100,
+		static_cast<int>(appContext.constants.sound.masterVolume),
+		resolution
+		);
+	m_slider->SetActive(true, appContext);
+	m_elements.push_back(m_slider);
+
+	m_acceptBtn = std::make_shared<ClassicButton>(
+		1,
+		GetElementPosition(m_pos, m_size, 0.5f, 0.95f),
+		GetElementSize(m_size, 0.2f, 0.15f),
+		Alignment::BOTTOM_MID,
+		resolution,
+		"Accept",
+		SoundType::ACCEPTED
+		);
+	m_acceptBtn->SetOnClick([this]() {
+		AppContext::GetInstance().eventManager.InvokeEvent(ClosePopUpEvent(this));
+		});
+	m_elements.push_back(m_acceptBtn);
+}
 
 InitialSoundLevelPopUp::InitialSoundLevelPopUp(Vector2 pos, Vector2 size,
 	Alignment alignment, Vector2 resolution, std::string const& title,
--- TentakelsAttacking2/UI/Elements/PopUp/public/InitialSoundLevelPopUp.h
+++ TentakelsAttacking2/UI/Elements/PopUp/public/InitialSoundLevelPopUp.h
@@ -6,8 +6,16 @@
 #pragma once
 #include "PopUp.h"
 
+class ClassicButton;
+class SliderAndInputLine;
+class CheckBox;
+
 class InitialSoundLevelPopUp : public PopUp {
 private:
+	std::shared_ptr<SliderAndInputLine> m_slider;
+	std::shared_ptr<CheckBox> m_checkBox;
+	std::shared_ptr<ClassicButton> m_acceptBtn;
+
 	void Initialize(Vector2 resolution);
 
 public:
