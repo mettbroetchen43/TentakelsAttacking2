77d91022c1fbed55bd11fdb0952a831fd2ef97c6
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-22 21:49:42 +0100

    doc Player

--- TentakelsAttacking2/Game/public/Player.h
+++ TentakelsAttacking2/Game/public/Player.h
@@ -6,17 +6,34 @@
 #pragma once
 #include "PlayerType.hpp"
 
+/**
+ * player class.
+ * contains id and type.
+ */
 class Player final {
 private:
-	unsigned int m_ID;
-	PlayerType m_playerType;
+	unsigned int m_ID; ///< contains the unique player id
+	PlayerType m_playerType; ///< contains the player type like HUMAN
 public:
+	/**
+	 * ctor.
+	 * only initialisation.
+	 */
 	Player(unsigned int ID, PlayerType playerType);
 
+	/**
+	 * returns if it is an human player.
+	 */
 	[[nodiscard]] bool IsHumanPlayer() const;
 
+	/**
+	 * returns the unique player id.
+	 */
 	[[nodiscard]] unsigned int GetID() const;
 
+	/**
+	 * compares 2 players by their id.
+	 */
 	friend bool operator== (Player const& lhs, Player const& rhs);
 };
 
