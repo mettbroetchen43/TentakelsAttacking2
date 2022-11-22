ee62f8bcdb3c255e08a36713b6fc64396c675e2b
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-22 19:09:39 +0100

    doc GeneralEvents

--- TentakelsAttacking2/Events/public/GenerelEvents.hpp
+++ TentakelsAttacking2/Events/public/GenerelEvents.hpp
@@ -11,6 +11,9 @@
 enum class GameEventType;
 class Galaxy;
 
+/**
+ * general event to provide player name and color.
+ */
 class PlayerEvent : public Event {
 private:
 	std::string m_name;
@@ -27,6 +30,9 @@ public:
 		return m_color;
 	}
 };
+/**
+ * general event to provide player ID, name and color.
+ */
 class PlayerWithIDEvent : public PlayerEvent {
 private:
 	unsigned int m_ID;
@@ -40,14 +46,23 @@ public:
 	}
 };
 
+/**
+ * use this to add a player in the locig.
+ */
 class AddPlayerEvent final : public PlayerEvent {
 public:
 	using PlayerEvent::PlayerEvent;
 };
+/**
+ * use this to edit a player in the locig.
+ */
 class EditPlayerEvent final : public PlayerWithIDEvent {
 public:
 	using PlayerWithIDEvent::PlayerWithIDEvent;
 };
+/**
+ * use this to delete a player by id in the locig.
+ */
 class DeletePlayerEvent final : public Event {
 private:
 	unsigned int m_ID;
@@ -59,16 +74,31 @@ public:
 		return m_ID;
 	}
 };
+/**
+ * use this to remove all player in locig.
+ */
 class ResetPlayerEvent final : public Event { };
 
+/**
+ * "callbackevent" from AddPlayerEvent.
+ * use this so add a player in PlayerCollection
+ */
 class AddPlayerUIEvent final : public PlayerWithIDEvent {
 public:
 	using PlayerWithIDEvent::PlayerWithIDEvent;
 };
+/**
+ * "callbackevent" from EditPlayerEvent.
+ * use this to edit a player in PlayerCollection
+ */
 class EditPlayerUIEvent final : public PlayerWithIDEvent {
 public:
 	using PlayerWithIDEvent::PlayerWithIDEvent;
 };
+/**
+ * "callbackevent" from DeletePlayerEvent.
+ * use this to delete a player in PlayerCollection
+ */
 class DeletePlayerUIEvent final : public Event {
 private:
 	unsigned int m_ID;
@@ -80,8 +110,15 @@ public:
 		return m_ID;
 	}
 };
+/**
+ * "callbackevent" from ResetPlayerEvent.
+ * use this remove all player from PlayerCollection
+ */
 class ResetPlayerUIEvent final : public Event { };
 
+/**
+ * general event to provide a player id.
+ */
 class PlayerIDEvent : public Event {
 private:
 	unsigned int m_ID;
@@ -93,20 +130,43 @@ public:
 		return m_ID;
 	}
 };
+/**
+ * use this to update the current player id in the UI.
+ */
 class UpdateCurrentPlayerIDEvent final : public PlayerIDEvent {
 public:
 	using PlayerIDEvent::PlayerIDEvent;
 };
+/**
+ * use this to update the next player id in the UI.
+ */
 class UpdateNextPlayerIDEvent final : public PlayerIDEvent {
 public:
 	using PlayerIDEvent::PlayerIDEvent;
 };
 
+/**
+ * use this to trigger the logic to evaluate the next term or round if no player remaining.
+ */
 class TriggerNextTermEvent final : public Event  { };
+/**
+ * use this to trigger the ui to show the next term.
+ */
 class ShowNextTermEvent final : public Event { };
+/**
+ * use this to trigger the ui to show the next round.
+ */
 class ShowNextRoundEvent final : public Event { };
 
+/**
+ * use this to trigger the logic to validate the Player count.
+ * return an ValidatePlayerCountResultEvent.
+ */
 class ValidatePlayerCountEvent final : public Event { };
+/**
+ * use this to return the value of ValidatePlayerCountEvent.
+ * ui should listen to that.
+ */
 class ValidatePlayerCountResultEvent final : public Event {
 private:
 	bool m_validPlayerCount;
@@ -120,8 +180,15 @@ public:
 	}
 };
 
+/**
+ * use this to trigger the logic to start the game.
+ */
 class StartGameEvent final : public Event { };
 
+/**
+ * use this to trigger the logic to updating the game events.
+ * returns a UpdateCheckGameEventsUI.
+ */
 class UpdateCheckGameEvent final : public Event {
 private:
 	GameEventType m_type;
@@ -138,6 +205,9 @@ public:
 		return m_isChecked;
 	}
 };
+/**
+ * use this to trigger the ui to update the game events.
+ */
 class UpdateCheckGameEventsUI final : public Event {
 private:
 	using Map = std::unordered_map<GameEventType, bool>const*;
@@ -151,8 +221,15 @@ public:
 		return m_types;
 	}
 };
+/**
+ * use this to trigger the logic to return the current state of the game events.
+ * returns a UpdateCheckGameEventsUI.
+ */
 class InitialCheckGameEventDataEvent final : public Event { };
 
+/**
+ * use this to trigger the constants to update the target last round. 
+ */
 class SetCurrentLastRoundEvent final : public Event {
 private:
 	int m_lastRound;
@@ -166,11 +243,28 @@ public:
 	}
 };
 
+/**
+ * use this to trigger the logic to generate a new galaxy.
+ */
 class GenerateGalaxyEvent final : public Event { };
+/**
+ * use this to trigger the logic to generate a new show galaxy.
+ */
 class GalaxyGeneratedUIEvent final : public Event { };
 
+/**
+ * use this to trigger the logic to return a galaxy pointer.
+ * returns a SendGalaxyPointerEvent.
+ */
 class GetGalaxyPointerEvent final : public Event { };
+/**
+ * use this to trigger the logic to return a show galaxy pointer.
+ * returns a SendGalaxyPointerEvent.
+ */
 class GetShowGalaxyPointerEvent final : public Event { };
+/**
+ * sends a galaxy pointer to the ui.
+ */
 class SendGalaxyPointerEvent final : public Event {
 private:
 	Galaxy const *const m_galaxy;
