1789af36454a8ffdeeb9ff5f885b4c5940f4dad6
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-22 17:12:27 +0100

    doc EventManager

--- TentakelsAttacking2/Events/public/EventManager.hpp
+++ TentakelsAttacking2/Events/public/EventManager.hpp
@@ -8,18 +8,32 @@
 #include <algorithm>
 #include <vector>
 
+
+/**
+ * manages the listener.
+ * provides all events to the listeners.
+ */
  class  EventManager final {
 public:
+	/**
+	 * adds a listener so it can receive all events.
+	 */
 	void AddListener(EventListener* eventListener) {
 		m_listeners.push_back(eventListener);
 	}
 
+	/**
+	 * revomes a listener so events gets no longer provided to this listener.
+	 */
 	void RemoveListener(EventListener* eventlistener) {
 		m_listeners.erase(std::remove(
 			m_listeners.begin(), m_listeners.end(), eventlistener),
 			m_listeners.end());
 	}
 
+	/**
+	 * provides the current event to all listeners. 
+	 */
 	void InvokeEvent(Event const& event) const {
 		for (auto const listener : m_listeners) {
 			listener->OnEvent(event);
@@ -27,5 +41,5 @@ public:
 	}
 
 private:
-	std::vector<EventListener*> m_listeners;
+	std::vector<EventListener*> m_listeners; ///< contains all active listeners
 };
