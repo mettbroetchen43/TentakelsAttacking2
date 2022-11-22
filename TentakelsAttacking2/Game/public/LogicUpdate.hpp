7ace2a6ab2687ef1c8f07dd4c92b626dc6caf249
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-22 20:46:44 +0100

    doc LogicUpdate

--- TentakelsAttacking2/Game/public/LogicUpdate.hpp
+++ TentakelsAttacking2/Game/public/LogicUpdate.hpp
@@ -5,6 +5,10 @@
 
 #pragma once
 
+/**
+ * provides pure virtual functions so that every object that is implementing this
+ * can be updated in the same way.
+ */
 struct LogicUpdate {
 	virtual void PreUpdate() = 0;
 	virtual void MainUpdate() = 0;
