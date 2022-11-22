232d1412e7b5a68607b75bc27fc5fdf0d5e8a22c
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-22 17:04:31 +0100

    doc EventListener

--- TentakelsAttacking2/Events/public/EventListener.hpp
+++ TentakelsAttacking2/Events/public/EventListener.hpp
@@ -6,8 +6,11 @@
 #pragma once
 #include "MainEvent.hpp"
 
+/**
+ * pure virtual class that should be implemenmted by every class that schould can receive events.
+ */
 class EventListener {
-	friend class EventManager;
+	friend class EventManager; ///< defines the event manager as friend so it can call OnEvent
 private:
-	virtual void OnEvent(Event const& event) = 0;
+	virtual void OnEvent(Event const& event) = 0; ///! gets implemented by every listener to receive events
 };
