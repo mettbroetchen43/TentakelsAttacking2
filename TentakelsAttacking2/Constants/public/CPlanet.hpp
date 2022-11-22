3e7287b9be08a2df175543dcca67233f6f5bb3b5
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-22 16:47:18 +0100

    doc CPlanet

--- TentakelsAttacking2/Constants/public/CPlanet.hpp
+++ TentakelsAttacking2/Constants/public/CPlanet.hpp
@@ -5,15 +5,18 @@
 
 #pragma once
 
+/**
+ * contains the planet constats
+ */
 struct CPlanet final {
-	size_t maxShips = 200;
-	size_t statringHumanShipsMultiplicator = 10; // multiply with production
-	size_t statringGlobalShipsMultiplicator = 5; // multiply with production
+	size_t maxShips = 200; ///< defines the max ships a non human player can have on one planet. (override by config)
+	size_t statringHumanShipsMultiplicator = 10; ///< defines the multiplicator to calculte the starting ships of human player. (override by config)
+	size_t statringGlobalShipsMultiplicator = 5; ///< defines the multiplicator to calculte the starting ships of npc player. (override by config)
 
-	size_t homeworldProduction = 10;
-	size_t minProduction = 3;
-	size_t maxProduction = 7;
+	size_t homeworldProduction = 10; ///< defines the production of a homeworld. (override by config)
+	size_t minProduction = 3; ///< defines the min production of a regular world. (override by config)
+	size_t maxProduction = 7; ///< defines the max production of a regular world. (override by config)
 
-	float homeworldSpacing = 0.15f;
-	float globalSpacing = 0.02f;
+	float homeworldSpacing = 0.15f; ///< defines the min spacing between homeworls. (override by config)
+	float globalSpacing = 0.02f; ///< defines the min spacing between regular worlds. (override by config)
 };
