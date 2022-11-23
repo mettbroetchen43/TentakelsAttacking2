dddafb6aff5b92fe45d1e0ca6da63fa98ff237f4
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-23 20:07:00 +0100

    SliderButton

--- TentakelsAttacking2/UI/Elements/Button/public/SliderButton.h
+++ TentakelsAttacking2/UI/Elements/Button/public/SliderButton.h
@@ -6,11 +6,18 @@
 #pragma once
 #include "Button.h"
 
+/**
+ * is used by the sliderclass as button to slide.
+ */
 class SliderButton final : public Button {
 public:
 	using Button::Button;
 	using Button::operator=;
 
+	/**
+	 * logic of the slider button.
+	 * calls ChackAndUpdate from button.
+	 */
 	void CheckAndUpdate(Vector2 const& mousePosition, AppContext const& appContext) override;
 
 };
