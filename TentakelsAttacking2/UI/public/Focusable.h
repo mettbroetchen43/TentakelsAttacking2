e0c9c54fded9f6dfb2b6f9a14418ce12abd2098f
martin <martin.schreurs@netcologne.de>
2022-11-25 08:48:24 +0100

    doc UI Focusable

--- TentakelsAttacking2/UI/public/Focusable.h
+++ TentakelsAttacking2/UI/public/Focusable.h
@@ -6,25 +6,57 @@
 #pragma once
 #include <raylib.h>
 
+/**
+ * provides the focusable inplementation for every ui element that should be focusable.
+ */
 class Focusable {
 private:
-	unsigned int m_focusID;
-	bool m_isFocus = false;
+	unsigned int m_focusID; ///< contains the unique (per layer) id
+	bool m_isFocus = false; ///< contains if this focusable is currntly in focus
 
 protected:
-	bool m_isNestedFocus = false;
+	bool m_isNestedFocus = false; ///< contains if there is a nested focus
 
 public:
+	/**
+	 * ctor.
+	 * only initialization.
+	 */
 	Focusable(unsigned int ID);
+	/**
+	 * dtor
+	 * deletes this in the focus system if its still exists.
+	 */
 	~Focusable();
 	
+	/**
+	 * returns if this element currently focused.
+	 */
 	[[nodiscard]] bool IsFocused() const;
+	/**
+	 * sets if this element is in focus.
+	 */
 	void SetFocus(bool focus);
+	/**
+	 * returns if this element has an nested focus.
+	 */
 	[[nodiscard]] bool IsNestedFocus() const;
+	/**
+	 * sets if this element has an nested focus.
+	 */
 	void SetNestedFocus(bool nestedFocus);
 
+	/**
+	 * returns if the current elements is enabled.
+	 */
 	[[nodiscard]] virtual bool IsEnabled() const = 0;
 
+	/**
+	 * returns the unique (per layer) id.
+	 */
 	[[nodiscard]] unsigned int GetFocusID() const;
+	/**
+	 * returns thr current collider.
+	 */
 	[[nodiscard]] virtual Rectangle GetCollider() const = 0;
 };
