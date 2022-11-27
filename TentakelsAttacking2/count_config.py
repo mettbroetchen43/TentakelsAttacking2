b9c5919352500483eb8ee703a0dfd312db899b20
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-11-27 22:23:41 +0100

    edit git irgnore

---
+++ TentakelsAttacking2/count_config.py
@@ -0,0 +1,18 @@
+#
+# Purpur Tentakel
+# count config
+# 17.11.2022
+#
+
+file_endings: list[str, ...] = [
+    ".cpp",
+    ".h",
+]
+
+error_files: list[str, ...] = [
+
+]
+
+single_line_comment: str = "//"
+multi_line_comment_begin: str = "/*"
+multi_line_comment_end: str = "*/"
