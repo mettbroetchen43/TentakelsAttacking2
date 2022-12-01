b940c2c2c5b906d122a71b8ebe06a6d4ea2f08e7
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-12-01 21:02:18 +0100

    Vector2 (Raylib) Helper

---
+++ TentakelsAttacking2/Helper/private/HVector2Operator.cpp
@@ -0,0 +1,18 @@
+//
+// Purpur Tentakel
+// 01.12.2022
+//
+
+#include "HVector2Operator.h"
+#include <cmath>
+
+Vector2 operator+(Vector2 const& lhs, Vector2 const& rhs) {
+    return { lhs.x + rhs.x, lhs.y + rhs.y };
+}
+Vector2 operator-(Vector2 const& lhs, Vector2 const& rhs) {
+    return { lhs.x - rhs.x, lhs.y - rhs.y };
+}
+
+float LenVec2(Vector2 const& value) {
+    return std::sqrt(value.x * value.x + value.y * value.y);
+}
---
+++ TentakelsAttacking2/Helper/public/HVector2Operator.h
@@ -0,0 +1,21 @@
+//
+// Purpur Tentakel
+// 01.12.2022
+//
+
+#pragma once
+#include <raylib.h>
+
+/**
+ * adds two Vector2 from Raylib.
+ */
+[[nodiscard]] Vector2 operator+ (Vector2 const& lhs, Vector2 const& rhs);
+/**
+ * subtracts two Vector2 from Raylib.
+ */
+[[nodiscard]] Vector2 operator- (Vector2 const& lhs, Vector2 const& rhs);
+
+/**
+ * returns the lenth of an Vector2 from Raylib.
+ */
+[[nodiscard]] float LenVec2(Vector2 const& value);
