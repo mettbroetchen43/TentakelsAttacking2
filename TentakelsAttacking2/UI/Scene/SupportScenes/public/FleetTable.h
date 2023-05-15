5bc96ea7d63782a404419437e8e3972b45cd168e
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-15 20:38:29 +0200

    add comments

--- TentakelsAttacking2/UI/Scene/SupportScenes/public/FleetTable.h
+++ TentakelsAttacking2/UI/Scene/SupportScenes/public/FleetTable.h
@@ -14,9 +14,19 @@ private:
 	Galaxy const* m_galaxy; ///< contains the current galaxy  of the logic
 	std::shared_ptr<Table> m_table; ///< contains the table within the planet infos
 
-	void Initialisation();
+	/**
+	 * initializes the table from the current galaxy.
+	 */
+	void Initialization();
 
 public:
+	/**
+	 * ctor and calls to initialize
+	 */
 	FleetTable(Vector2 pos, Vector2 size, Alignment alignment, Vector2 resolution, Galaxy const* galaxy);
+	/**
+	 * sets if the scene is currently active.
+	 * calls set focus active.
+	 */
 	void SetActive(bool active, AppContext const& appContext) override;
 };
