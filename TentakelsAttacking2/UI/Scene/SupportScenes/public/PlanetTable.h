d83edd3dc36f4083a7cf317cf8ba24ae00d87913
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-15 20:39:44 +0200

    add comments

--- TentakelsAttacking2/UI/Scene/SupportScenes/public/PlanetTable.h
+++ TentakelsAttacking2/UI/Scene/SupportScenes/public/PlanetTable.h
@@ -13,12 +13,14 @@ private:
 	Galaxy const* m_galaxy; ///< contains the current galaxy  of the logic
 	std::shared_ptr<Table> m_table; ///< contains the table within the planet infos
 
-	void Initialisation();
+	/**
+	 * initializes the table from the current galaxy.
+	 */
+	void Initialization();
 
 public:
 	/**
-	 * ctor.
-	 * only initialization.
+	 * ctor and calls to initialize
 	 */
 	PlanetTable(Vector2 pos, Vector2 size, Alignment alignment, Vector2 resolution,
 		Galaxy const* galaxy);
