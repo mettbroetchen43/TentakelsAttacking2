71010836b4e2c0df0bf2368e891cf1c843dd0562
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-19 00:16:17 +0200

    Fleet.cpp

--- TentakelsAttacking2/Game/private/Fleet.cpp
+++ TentakelsAttacking2/Game/private/Fleet.cpp
@@ -7,11 +7,11 @@
 
 Fleet::Fleet(unsigned int ID, vec2pos position, std::shared_ptr<Player> player,
     std::shared_ptr<SpaceObject> target)
-    : SpaceObject(ID, position, player), m_target(target) { }
+    : SpaceObject{ ID, position, player }, m_target{ target } { }
 
 Fleet::Fleet(unsigned int ID, vec2pos position, size_t ships, std::shared_ptr<Player> player,
     std::shared_ptr<SpaceObject> target)
-    : SpaceObject(ID, position, ships, player), m_target(target){ }
+    : SpaceObject{ ID, position, ships, player }, m_target{ target } { }
 
 bool Fleet::IsFleet() const {
     return true;
