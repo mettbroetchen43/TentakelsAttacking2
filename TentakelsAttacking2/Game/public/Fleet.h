e4cc95874ee0dad68fce9a3aac0e1ef74ee4f691
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-19 00:44:49 +0200

    Fleet.h

--- TentakelsAttacking2/Game/public/Fleet.h
+++ TentakelsAttacking2/Game/public/Fleet.h
@@ -6,25 +6,25 @@
 #pragma once
 #include "SpaceObject.h"
 
-class Fleet : public SpaceObject {
+class Fleet final : public SpaceObject {
 private:
-	std::shared_ptr<SpaceObject> m_target;
+	std::shared_ptr<SpaceObject> m_target{ nullptr };
 
 public:
 	/**
 	 * ctor without ships.
-	 * only initialisation
+	 * only initialization
 	 */
 	Fleet(unsigned int ID, vec2pos position, std::shared_ptr<Player> player, std::shared_ptr<SpaceObject> target);
 	/**
 	 * ctor.
-	 * only initialisation.
+	 * only initialization.
 	 */
 	Fleet(unsigned int ID, vec2pos position, size_t ships, std::shared_ptr<Player> player,
 		std::shared_ptr<SpaceObject> target);
 
 	/**
-	 * returns if the current spaceobject is a planet.
+	 * returns if the current space object is a planet.
 	 */
 	[[nodiscard]] bool IsFleet() const override;
 
