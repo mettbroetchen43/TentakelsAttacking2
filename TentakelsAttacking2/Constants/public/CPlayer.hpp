52d6d0357208bd33d300d80ccb0587c4562fb4f0
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-18 23:45:44 +0200

    CPlayer.hpp

--- TentakelsAttacking2/Constants/public/CPlayer.hpp
+++ TentakelsAttacking2/Constants/public/CPlayer.hpp
@@ -9,6 +9,6 @@
  * contains the player constants
  */
 struct CPlayer final {
-	size_t minPlayerCount = 2; ///< defines the min player neded for a game (override by config)
-	size_t maxPlayerCount = 7; ///< defines the max playerslots available for one game (override by config)
+	size_t minPlayerCount{ 2 }; ///< defines the min player needed for a game (override by config)
+	size_t maxPlayerCount{ 7 }; ///< defines the max player slots available for one game (override by config)
 };
