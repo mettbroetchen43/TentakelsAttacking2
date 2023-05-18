d68c3783600b686f45ecc7984478b7ef9e239421
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-18 23:46:40 +0200

    CSound.hpp

--- TentakelsAttacking2/Constants/public/CSound.hpp
+++ TentakelsAttacking2/Constants/public/CSound.hpp
@@ -6,9 +6,9 @@
 #pragma once
 
 /**
- * conatins all sound constants
+ * contains all sound constants
  */
 struct CSound final {
-	bool muteVolume = false; ///< contains if the game is muted (override by config, true == mute)
-	float masterVolume = 50.0f; ///< contains the current master volume (override by config)
+	bool muteVolume{ false }; ///< contains if the game is muted (override by config, true == mute)
+	float masterVolume{ 50.0f }; ///< contains the current master volume (override by config)
 };
