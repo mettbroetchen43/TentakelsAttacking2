77cccc547f35028c9c2a0ca55ebbc02f7a7c115d
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-20 00:46:16 +0200

    FleetTable.h

--- TentakelsAttacking2/UI/Scene/SupportScenes/public/FleetTable.h
+++ TentakelsAttacking2/UI/Scene/SupportScenes/public/FleetTable.h
@@ -12,7 +12,7 @@ class Table;
 class FleetTable final : public Scene {
 private:
 	Galaxy const* m_galaxy; ///< contains the current galaxy  of the logic
-	std::shared_ptr<Table> m_table; ///< contains the table within the planet infos
+	std::shared_ptr<Table> m_table; ///< contains the table within the planet info's
 
 	/**
 	 * initializes the table from the current galaxy.
