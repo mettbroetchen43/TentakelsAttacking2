d5b0ab5a9a73beff7cadc6acb684f7573abfda98
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-20 00:48:05 +0200

    GameEventSettings.h

--- TentakelsAttacking2/UI/Scene/SupportScenes/public/GameEventSettings.h
+++ TentakelsAttacking2/UI/Scene/SupportScenes/public/GameEventSettings.h
@@ -12,7 +12,7 @@ class CheckBox;
 enum class GameEventTypes;
 
 /**
- * provides a scene where the game evenets can be set.
+ * provides a scene where the game events can be set.
  */
 class GameEventSettings : public Scene, public EventListener {
 private:
@@ -30,12 +30,12 @@ private:
 	void SetChecked(unsigned int ID, bool isCecked);
 
 	/**
-	 * sets the checksboxes when logic calls it via event.
+	 * sets the checkboxes when logic calls it via event.
 	 * calls set global checkbox.
 	 */
 	void UpdateElements(UpdateCheckGameEventsUI const* event);
 	/**
-	 * calulates if the global checkbox should be set.
+	 * calculates if the global checkbox should be set.
 	 * updates the checkbox.
 	 */
 	void SetGlobalCheckbox();
@@ -44,23 +44,23 @@ public:
 	/**
 	 * ctor.
 	 * add an event listener.
-	 * triggers logic to send the initial dasa via event.
+	 * triggers logic to send the initial data via event.
  	 */
 	GameEventSettings(unsigned int focusID, Vector2 pos, Vector2 size, Alignment alignment,
 		Vector2 resolution);
 	/**
 	 * dtor.
-	 * removes an eventr listener.
+	 * removes an event listener.
 	 */
 	~GameEventSettings();
 
 	/**
-	 * receives all events and calles the member functions.
+	 * receives all events and calls the member functions.
 	 */
 	void OnEvent(Event const& event);
 
 	/**
-	 * sets random values for every checkbox except for the gloabl checkpox.
+	 * sets random values for every checkbox except for the global checkbox.
 	 * calls global checkbox to set.
 	 */
 	void SetRandom();
