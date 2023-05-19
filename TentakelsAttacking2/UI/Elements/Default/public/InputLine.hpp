7b00db49a0f847dee0438961b6a653a230451376
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-19 16:16:07 +0200

    InputLine.hpp

--- TentakelsAttacking2/UI/Elements/Default/public/InputLine.hpp
+++ TentakelsAttacking2/UI/Elements/Default/public/InputLine.hpp
@@ -18,28 +18,28 @@
 template <class T>
 class InputLine final : public UIElement, public Focusable {
 protected:
-	bool m_isEnabled = true; ///< contains if the inputline is enabled
-	unsigned int m_charLimit; ///< contains the max abount of chars in the inputline
+	bool m_isEnabled{ true }; ///< contains if the input line is enabled
+	unsigned int m_charLimit; ///< contains the max about of chars in the input line
 	std::string m_value; ///< contains the current value
 	std::string m_oldValue; ///< contains the old value
 	std::string m_placeholderText; ///< contains the placeholder text -> gets showed when no value is provided
-	Texture* m_texture; ///< contauns the background texture
-	double m_backspacePressTime = 0.0; ///< contains the time since the last time backspace got pressed and still hold
-	std::function<void()> m_onEnter = []() {}; ///< contains the onEnter lambda gets called then enter is pressed
-	std::function<void()> m_onValueChanced = []() {}; ///< contains the lambda that gets calles when the value chances.
+	Texture* m_texture; ///< contains the background texture
+	double m_backspacePressTime{ 0.0 }; ///< contains the time since the last time backspace got pressed and still hold
+	std::function<void()> m_onEnter{ []() {} }; ///< contains the onEnter lambda gets called then enter is pressed
+	std::function<void()> m_onValueChanced{ []() {} }; ///< contains the lambda that gets calles when the value chances.
 
 	/**
-	 * checks wether the inputline is still full.
-	 * if not the char is edded.
-	 * returns if a char was edded.
+	 * checks weather the input line is still full.
+	 * if not the char is added.
+	 * returns if a char was added.
 	 */
 	bool AddChar(int key) {
-		bool validAdd = m_charLimit > m_value.size();
+		bool const validAdd{ m_charLimit > m_value.size() };
 
 		if (validAdd) {
 			m_value += static_cast<char>(key);
 
-			auto event = PlaySoundEvent(SoundType::TEXT);
+			PlaySoundEvent const event { SoundType::TEXT };
 			AppContext::GetInstance().eventManager.InvokeEvent(event);
 
 			m_onValueChanced();
@@ -48,14 +48,14 @@ protected:
 		return validAdd;
 	}
 	/**
-	 * removes a char if the inputline is not empty.
+	 * removes a char if the input line is not empty.
 	 * if it is empty: nothing happens.
 	 */
 	void RemoveChar() {
 		if (m_value.size() != 0) {
 			m_value.pop_back();
 
-			auto event = PlaySoundEvent(SoundType::TEXT);
+			PlaySoundEvent const event { SoundType::TEXT };
 			AppContext::GetInstance().eventManager.InvokeEvent(event);
 
 			m_onValueChanced();
@@ -63,7 +63,7 @@ protected:
 	}
 	/**
 	 * gets deleted here because it is overloaded for each of the provided datatypes.
-	 * checks if a spacific key is a valid key.
+	 * checks if a specific key is a valid key.
 	 */
 	[[nodiscard]] bool IsValidKey(int key) = delete;
 
@@ -74,7 +74,7 @@ public:
 	 */
 	InputLine(unsigned int focusID, Vector2 pos, Vector2 size, Alignment alignment,
 		Vector2 resolution, unsigned int charLimit)
-		: UIElement(pos, size, alignment, resolution), Focusable(focusID), m_charLimit(charLimit) {
+		: UIElement{ pos, size, alignment, resolution }, Focusable{ focusID }, m_charLimit{ charLimit } {
 		m_texture = AppContext::GetInstance().assetManager.GetTexture(AssetType::GREY);
 	}
 	InputLine(InputLine const&) = default;
@@ -83,14 +83,14 @@ public:
 	InputLine& operator= (InputLine&&) = default;
 
 	/**
-	 * returs if the inputline is enabled.
+	 * returns if the input line is enabled.
 	 */
 	[[nodiscard]] virtual bool IsEnabled() const override {
 		return true;
 	}
 
 	/**
-	 * logic of the inputline.
+	 * logic of the input line.
 	 */
 	void CheckAndUpdate(Vector2 const& mousePosition, AppContext const& appContext) override {
 
@@ -98,18 +98,21 @@ public:
 
 		if (!m_isEnabled) { return; }
 
-		bool hover = CheckCollisionPointRec(mousePosition, m_collider);
-		bool validSelect = !IsFocused() and
-			hover and IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+		bool const hover{ CheckCollisionPointRec(mousePosition, m_collider) };
+		bool const validSelect{
+			    !IsFocused()
+			and hover
+			and IsMouseButtonPressed(MOUSE_LEFT_BUTTON) 
+		};
 
 		if (validSelect) {
-			auto event = SelectFocusElementEvent(this);
+			SelectFocusElementEvent const event { this };
 			appContext.eventManager.InvokeEvent(event);
 		}
 
 		if (!IsFocused()) { return; }
 
-		bool enter = IsOnlyEnterConfirmInputReleased();
+		bool const enter{ IsOnlyEnterConfirmInputReleased() };
 		if (enter) {
 			m_onEnter();
 		}
@@ -128,26 +131,26 @@ public:
 		}
 
 		while (true) {
-			int key = GetCharPressed();
+			int const key{ GetCharPressed() };
 
 			if (key <= 0) { break; }
 
 			if (!IsValidKey(key)) { continue; }
 
 			if (!AddChar(key)) {
-				auto event = ShowMessagePopUpEvent("Max Input", "Maximum number of input values reached");
+				ShowMessagePopUpEvent const event{ "Max Input", "Maximum number of input values reached" };
 				appContext.eventManager.InvokeEvent(event);
 				break;
 			}
 		}
 	}
 	/**
-	 * renders the inputline
+	 * renders the input line
 	 */
 	void Render(AppContext const& appContext) override {
 
-		auto constants = appContext.constants.textProcessing;
-		// Update here to make shure its after call of HasValueChanced();
+		auto const constants{ appContext.constants.textProcessing };
+		// Update here to make sure its after call of HasValueChanced();
 		m_oldValue = m_value;
 
 		Rectangle textureRec = {
@@ -174,10 +177,10 @@ public:
 			);
 		}
 
-		float posX = m_collider.x + 10.0f;
-		float posY = m_collider.y + m_collider.height * 0.1f;
-		float fontSize = m_collider.height * 0.8f;
-		std::string printableInput;
+		float const posX{ m_collider.x + 10.0f };
+		float const posY{ m_collider.y + m_collider.height * 0.1f };
+		float const fontSize{ m_collider.height * 0.8f };
+		std::string printableInput{ "" };
 
 		if (m_value.size() > 0) {
 			printableInput = GetPrintableTextInCollider(
@@ -196,12 +199,12 @@ public:
 			);
 		}
 		else {
-			std::string printablePlaceholder = GetPrintablePlaceholderTextInCollider(
+			std::string const printablePlaceholder{ GetPrintablePlaceholderTextInCollider(
 				m_placeholderText,
 				fontSize,
 				m_collider,
 				appContext
-			);
+			) };
 			DrawTextEx(
 				*(appContext.assetManager.GetFont()),
 				printablePlaceholder.c_str(),
@@ -214,7 +217,7 @@ public:
 
 
 		if (IsFocused()) {
-			size_t time = static_cast<size_t>(GetTime() * 2.0);
+			size_t const time{ static_cast<size_t>(GetTime() * 2.0) };
 			Vector2 textLength = MeasureTextEx(
 				*(appContext.assetManager.GetFont()),
 				printableInput.c_str(),
@@ -240,20 +243,20 @@ public:
 
 	/**
 	 * sets a new placeholder text.
-	 * it will be renderd if the inputline is empty.
+	 * it will be rendered if the input line is empty.
 	 */
 	void SetPlaceholderText(std::string placeholderText) {
 		m_placeholderText = placeholderText;
 	}
 	/**
-	 * sets a value into the inputline.
+	 * sets a value into the input line.
 	 */
 	void SetValue(T value) {
 		m_value = std::to_string(value);
 		m_onValueChanced();
 	}
 	/**
-	 * extens the current value.
+	 * extends the current value.
 	 * adds the provided text an the end oif the current text.
 	 */
 	void ExtendValue(T value) {
@@ -266,7 +269,7 @@ public:
 	 */
 	[[nodiscard]] T GetValue() = delete;
 	/**
-	 * clears the inputline.
+	 * clears the input line.
 	 */
 	void Clear() {
 		m_value.clear();
@@ -280,7 +283,7 @@ public:
 		return UIElement::GetCollider();
 	}
 	/**
-	 * returns if the inputline has any value.
+	 * returns if the input line has any value.
 	 */
 	[[nodiscard]] bool HasValue() const {
 		return !m_value.empty();
@@ -298,14 +301,14 @@ public:
 		m_onEnter = onEnter;
 	}
 	/**
-	 * sets the onValueChanced lambda that is called when the value of the inputline chanced.
+	 * sets the onValueChanced lambda that is called when the value of the input line chanced.
 	 */
-	void SetOnValueChanced(std::function<void()> onValueChaned) {
-		m_onValueChanced = onValueChaned;
+	void SetOnValueChanced(std::function<void()> onValueChanged) {
+		m_onValueChanced = onValueChanged;
 	}
 
 	/**
-	 * sets if the inputline is enabled.
+	 * sets if the input line is enabled.
 	 */
 	void SetEnabled(bool isEnabled) {
 		m_isEnabled = isEnabled;
@@ -313,23 +316,23 @@ public:
 };
 
 /**
- * checks if its a valid key for a int inputline.
+ * checks if its a valid key for a int input line.
  */
 template<>
 inline bool InputLine<int>::IsValidKey(int key) {
-	bool valid = key >= 48 and
-		key <= 57;
-
-	return valid;
+	return {
+			key >= 48
+		and key <= 57 
+	};
 }
 /**
- * checks if its a valid key for a float inputline.
+ * checks if its a valid key for a float input line.
  */
 template<>
 inline bool InputLine<float>::IsValidKey(int key) {
-	bool valid = (key >= 48 and key <= 57) // numbers
+	bool valid{ (key >= 48 and key <= 57) // numbers
 		or key == 44 // comma
-		or key == 46; // dot
+		or key == 46 }; // dot
 
 	//check for multiple commas/dots
 	if ('.' == key or ',' == key) {
@@ -337,7 +340,7 @@ inline bool InputLine<float>::IsValidKey(int key) {
 			valid = false;
 		}
 
-		for (char c : m_value) {
+		for (char const &c : m_value) {
 			if ('.' == c or ',' == c) {
 				valid = false;
 				break;
@@ -348,13 +351,13 @@ inline bool InputLine<float>::IsValidKey(int key) {
 	return valid;
 }
 /**
- * checks if its a valid key for a double inputline.
+ * checks if its a valid key for a double input line.
  */
 template<>
 inline bool InputLine<double>::IsValidKey(int key) {
-	bool valid = (key >= 48 and key <= 57) // numbers
+	bool valid{ (key >= 48 and key <= 57) // numbers
 		or key == 44 // comma
-		or key == 46; // dot
+		or key == 46 }; // dot
 
 	//check for multiple commas/dots
 	if ('.' == key or ',' == key) {
@@ -362,7 +365,7 @@ inline bool InputLine<double>::IsValidKey(int key) {
 			valid = false;
 		}
 
-		for (char c : m_value) {
+		for (char const& c : m_value) {
 			if ('.' == c or ',' == c) {
 				valid = false;
 				break;
@@ -373,14 +376,14 @@ inline bool InputLine<double>::IsValidKey(int key) {
 	return valid;
 }
 /**
- * checks if its a valid key for a string inputline.
+ * checks if its a valid key for a string input line.
  */
 template<>
 inline bool InputLine<std::string>::IsValidKey(int key) {
-	bool valid = key >= 32 and
-		key <= 126;
-
-	return valid;
+	return {
+			key >= 32
+		and key <= 126 
+	};
 }
 
 /**
@@ -403,7 +406,7 @@ template<>
 	if (m_value.size() == 0) {
 		return 0.0f;
 	}
-	for (char& c : m_value) {
+	for (char c : m_value) {
 		if (c == ',') {
 			c = '.';
 			break;
@@ -439,7 +442,7 @@ template<>
 
 /**
  * sets the value for a string input line.
- * need of this overload because std::to_string doesnt provides an overloas for an std::string.
+ * need of this overload because std::to_string doesn't provides an overload for an std::string.
  */
 template<>
 inline void InputLine<std::string>::SetValue(std::string value) {
@@ -448,7 +451,7 @@ inline void InputLine<std::string>::SetValue(std::string value) {
 }
 /**
  * sets the value for a string input line.
- * need of this overload because std::to_string doesnt provides an overloas for an std::string.
+ * need of this overload because std::to_string doesn't provides an overload for an std::string.
  */
 template<>
 inline void InputLine<std::string>::ExtendValue(std::string value) {
