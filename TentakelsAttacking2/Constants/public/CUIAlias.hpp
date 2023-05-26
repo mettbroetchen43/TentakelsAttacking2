62f1b434c63286f237074632f02be9c1fb7cac78
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-26 15:09:44 +0200

    Arrow

--- TentakelsAttacking2/Constants/public/CUIAlias.hpp
+++ TentakelsAttacking2/Constants/public/CUIAlias.hpp
@@ -10,6 +10,7 @@ using AppContext_ty = AppContext&;
 using AppContext_ty_c = AppContext const&;
 
 // Elements
+	// Button
 class ClassicButton;
 using ClassicButton_ty = std::shared_ptr<ClassicButton>;
 using ClassicButton_ty_c = std::shared_ptr<ClassicButton> const&;
@@ -18,6 +19,7 @@ class SliderButton;
 using SliderButton_ty = std::shared_ptr<SliderButton>;
 using SliderButton_ty_c = std::shared_ptr<SliderButton> const&;
 
+	// Color Picker
 class ColorPicker;
 using ColorPicker_ty = std::shared_ptr<ColorPicker>;
 using ColorPicker_ty_c = std::shared_ptr<ColorPicker> const&;
@@ -25,3 +27,7 @@ using ColorPicker_ty_c = std::shared_ptr<ColorPicker> const&;
 class ColorPickerCell;
 using ColorPickerCell_ty = std::shared_ptr<ColorPickerCell>;
 using ColorPickerCell_ty_c = std::shared_ptr<ColorPickerCell> const&;
+	// Default
+class Arrow;
+using Arrow_ty = std::shared_ptr<Arrow>;
+using Arrow_ty_c = std::shared_ptr<Arrow> const&;
