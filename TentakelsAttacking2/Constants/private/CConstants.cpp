73d6464515ce46592e2939d2fd871eee12a871e0
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-09 17:41:53 +0200

    calculate global config count

---
+++ TentakelsAttacking2/Constants/private/CConstants.cpp
@@ -0,0 +1,19 @@
+//
+// Purpur Tentakel
+// 09.06.2023
+//
+
+#include "CConstants.h"
+
+int Constants::GetConfigValueCount() const {
+    return
+        global.configVersionCount +
+        fight.configEntryCount +
+        fleet.configEntryCount +
+        global.configEntryCount +
+        planet.configEntryCount +
+        player.configEntryCount +
+        sound.configEntryCount +
+        window.configEntryCount +
+        world.configEntryCount;
+}
