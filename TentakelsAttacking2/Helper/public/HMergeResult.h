61104a16f4786287a92ebed931d15294178fc87a
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-09 19:31:42 +0200

    moved member

--- TentakelsAttacking2/Helper/public/HMergeResult.h
+++ TentakelsAttacking2/Helper/public/HMergeResult.h
@@ -7,16 +7,17 @@
 #include "HLogicAlias.hpp"
 
 struct HMergeResult final {
+private:
+	Player_ty m_player;
+	SpaceObject_ty m_origin;
+	SpaceObject_ty m_destination;
+	int m_count;
+
+public:
 	HMergeResult(Player_ty player, SpaceObject_ty origin, SpaceObject_ty destination, int count);
 
 	[[nodiscard]] Player_ty GetPlayer() const;
 	[[nodiscard]] SpaceObject_ty GetOrigin() const;
 	[[nodiscard]] SpaceObject_ty GetDestination() const;
 	[[nodiscard]] int GetCount() const;
-
-private:
-	Player_ty m_player;
-	SpaceObject_ty m_origin;
-	SpaceObject_ty m_destination;
-	int m_count;
 };
