376f0bf1a7c65d6c6cada7775d02b284562254a2
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-12 18:11:38 +0200

    improved range

--- TentakelsAttacking2/Constants/public/CFight.hpp
+++ TentakelsAttacking2/Constants/public/CFight.hpp
@@ -8,5 +8,5 @@
 struct CFight final {
 	static inline int const configEntryCount{ 2 }; ///< config load checks if the count is idetical to the loaded entry count
 	float hitChance{ 0.3f }; ///< contains the change a single ship will hit. gets overwritten by config
-	int fleetFightRange{ 3 }; ///< contains the range 2 fleets will fight. get ovberwritten by config.
+	int fleetFightRange{ 4 }; ///< contains the range 2 fleets will fight. get ovberwritten by config.
 };
