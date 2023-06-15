99d10aea51829c593baed383eca3298835733cef
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-15 22:02:00 +0200

    added a UI Alias

--- TentakelsAttacking2/Helper/public/HUIAlias.hpp
+++ TentakelsAttacking2/Helper/public/HUIAlias.hpp
@@ -87,6 +87,10 @@ class UIGalaxy;
 using UIGalaxy_ty =   std::shared_ptr<UIGalaxy>;
 using UIGalaxy_ty_c = std::shared_ptr<UIGalaxy> const&;
 
+class UIGalaxyElement;
+using UIGalaxyElement_ty = std::shared_ptr<UIGalaxyElement>;
+using UIGalaxyElement_ty_c = std::shared_ptr<UIGalaxyElement> const&;
+
 class UIPlanet;
 using UIPlanet_ty =   std::shared_ptr<UIPlanet>;
 using UIPlanet_ty_c = std::shared_ptr<UIPlanet> const&;
