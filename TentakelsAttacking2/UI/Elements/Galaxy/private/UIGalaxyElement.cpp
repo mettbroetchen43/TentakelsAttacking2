7285acabc215108f894b664f6f363a02b72e35d7
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-17 12:03:58 +0200

    format

--- TentakelsAttacking2/UI/Elements/Galaxy/private/UIGalaxyElement.cpp
+++ TentakelsAttacking2/UI/Elements/Galaxy/private/UIGalaxyElement.cpp
@@ -10,8 +10,7 @@ UIGalaxyElement::UIGalaxyElement(unsigned int focusID, unsigned int ID, Vector2 
 	Vector2 resolution, Vector2 colliderPos)
 	: Focusable{ focusID }, UIElement{ pos, size, Alignment::MID_MID, resolution }
 	, m_ID{ ID }, m_currentPlayer{ player }, m_colliderPos{ colliderPos }, m_color{ player.color }, m_stringID {
-	std::to_string(ID)
-},
+	std::to_string(ID) },
 	m_hover{
 		0.03f,
 		"",
