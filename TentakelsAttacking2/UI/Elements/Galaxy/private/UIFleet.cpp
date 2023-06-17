3741d0470e89ee0fee0bec0977f954443b3e2b72
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-17 12:04:13 +0200

    implemented fleet

---
+++ TentakelsAttacking2/UI/Elements/Galaxy/private/UIFleet.cpp
@@ -0,0 +1,68 @@
+//
+// Purpur Tentakel
+// 16.06.2023
+//
+
+#include "UIFleet.h"
+#include "Fleet.h"
+
+bool UIFleet::IsColliding(Vector2 const& mousePosition) const {
+    auto const& lineStart{ m_line.GetStart() };
+    auto const& lineEnd  { m_line.GetEnd()   };
+    Vector2 const start{
+        lineStart.x * m_resolution.x,
+        lineStart.y * m_resolution.y
+    };
+    Vector2 const end{
+        lineEnd.x * m_resolution.x,
+        lineEnd.y * m_resolution.y,
+    };
+
+    return CheckCollisionPointLine(mousePosition, start, end, 2);
+}
+
+UIFleet::UIFleet(PlayerData player, Vector2 start, Vector2 end, Vector2 resolution, Fleet_ty_raw_c fleet)
+    : UIElement{ start, { 0.0f,0.0f }, Alignment::MID_MID, resolution }, m_player{ player }, m_fleet{ fleet },
+    m_line{
+        start,
+        end,
+        resolution,
+        1.0f,
+        player.color
+    },
+    m_hover{
+        0.03f,
+        "",
+        player.color,
+        Vector2(0.01f,0.01f),
+        resolution
+    } {
+    UpdateHoverText();
+}
+
+void UIFleet::UpdateHoverText() {
+    std::string const text{ std::to_string(m_fleet->GetID()) + " | x: " + std::to_string(m_fleet->GetPos().x) +
+        " x: " + std::to_string(m_fleet->GetPos().y) + " | ships: " + std::to_string(m_fleet->GetShipCount()) };
+    m_hover.SetText(text);
+}
+void UIFleet::UpdatePositions(Vector2 start, Vector2 end) {
+    m_line.SetStart(start);
+    m_line.SetEnd(end);
+}
+
+void UIFleet::CheckAndUpdate(Vector2 const& mousePosition, AppContext_ty_c appContext) {
+    UIElement::CheckAndUpdate(mousePosition, appContext);
+
+    if (IsColliding(mousePosition)) {
+        UpdateHoverText();
+        m_hover.SetRenderHover(mousePosition, appContext);
+    }
+}
+void UIFleet::Render(AppContext_ty_c appContext) {
+    m_line.Render(appContext);
+}
+void UIFleet::Resize(Vector2 resolution, AppContext_ty_c appContext) {
+    UIElement::Resize(resolution, appContext);
+    m_line.Resize(resolution, appContext);
+    m_hover.Resize(resolution, appContext);
+}
---
+++ TentakelsAttacking2/UI/Elements/Galaxy/public/UIFleet.h
@@ -0,0 +1,52 @@
+//
+// Purpur Tentakel
+// 16.06.2023
+//
+
+#pragma once
+#include "UIElement.hpp"
+#include "Line.h"
+#include "HPlayerCollection.h"
+#include "HLogicAlias.hpp"
+#include "Hover.h"
+
+class UIFleet final : public UIElement {
+private:
+	PlayerData m_player; ///< contains the current player data
+	Fleet_ty_raw_c m_fleet; ///< contains the logic Planet to this Planet
+	Line m_line; ///< contains the line for displaying
+	Hover m_hover; ///< contains the hover Element
+
+	/**
+	 * checks if a point is colliding with the line.
+	 */
+	[[nodiscard]] bool IsColliding(Vector2 const& mousePosition) const;
+
+public:
+	UIFleet(PlayerData player, Vector2 start, Vector2 end, Vector2 resolution, Fleet_ty_raw_c fleet);
+	
+	/**
+	 * updates the hover text.
+	 */
+	void UpdateHoverText();
+
+	/**
+	 * updates the start and end positions
+	 */
+	void UpdatePositions(Vector2 start, Vector2 end);
+
+	/**
+	 * calls the UIElement to Update.
+	 * checks collision with line.
+	 */
+	virtual void CheckAndUpdate(Vector2 const&, AppContext_ty_c appContext);
+	/**
+	 * renders the line.
+	 */
+	void Render(AppContext_ty_c appContext) override;
+	/**
+	 * calls UIElement to Resize.
+	 * Resizes the line.
+	 */
+	void Resize(Vector2 resolution, AppContext_ty_c appContext) override;
+};
