baa42893f17aad688036ffa76ea3ff3eb7bd869f
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-20 20:36:12 +0200

    rename

--- TentakelsAttacking2/Constants/public/CGlobal.hpp
+++ TentakelsAttacking2/Constants/public/CGlobal.hpp
@@ -14,7 +14,7 @@ struct CGlobal final {
 	static inline int const configVersionCount{ 2 }; ///< config load checks if the count is idetical to the loaded version count
 	static inline int const configSectionCount{ 9 }; ///< config load checks if the count is idetical to the loaded section count
 
-	std::string current_language_name{ "english" }; ///< contains the current language name (override by config)
+	std::string currentLanguageName{ "english" }; ///< contains the current language name (override by config)
 
 	static inline std::string const configVersion{ "v0.1.0.1" }; ///< contains the config version string
 	// gameVersion will be extracted with regex in the GitHub CI. Don't chance the following line. see CMakeList.txt
