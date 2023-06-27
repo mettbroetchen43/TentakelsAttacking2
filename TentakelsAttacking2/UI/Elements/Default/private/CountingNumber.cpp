503888f5591e7a8f1b88c3b62cbe8790f085fce4
Martin Schreurs <martin.schreurs@netcologne.de>
2023-06-27 16:57:36 +0200

    let numbers count

--- TentakelsAttacking2/UI/Elements/Default/private/CountingNumber.cpp
+++ TentakelsAttacking2/UI/Elements/Default/private/CountingNumber.cpp
@@ -7,9 +7,61 @@
 #include "AppContext.h"
 #include "Text.h"
 
+void CountingNumber::HandleCounting() {
+	if (m_currentNumber != m_targetNumber and not m_isCounting) {
+		m_isCounting = true;
+		m_startCountingTime = GetTime();
+	}
+	if (not m_isCounting) { return; }
+
+	switch (m_countingType) {
+		default:
+		case LINEAR:
+			HandleLinearCounting();
+			break;
+		case ASYMPTOTIC:
+			HandleAsymptoticCounting();
+			break;
+	}
+
+	if ((m_targetNumber - m_startNumber) >= 0) {
+		if (m_targetNumber < m_currentNumber) { m_currentNumber = m_targetNumber; }
+	}
+	else {
+		if (m_targetNumber > m_currentNumber) { m_currentNumber = m_targetNumber; }
+	}
+
+	if (m_currentNumber == m_targetNumber and m_isCounting) {
+		m_isCounting = false;
+		m_callback(m_countingType, m_startNumber, m_currentNumber, m_timeInS);
+	}
+}
+void CountingNumber::HandleLinearCounting() {
+	double const percent{ (GetTime() - m_startCountingTime) / m_timeInS };
+	int nextNumber = { m_startNumber + static_cast<int>((m_targetNumber - m_startNumber) * percent) };
+
+	SetNewNumber(nextNumber);
+}
+void CountingNumber::HandleAsymptoticCounting() {
+	double const percent{ std::sqrt((GetTime() - m_startCountingTime) / m_timeInS) };
+	int nextNumber = { m_startNumber + static_cast<int>((m_targetNumber - m_startNumber) * percent) };
+
+	SetNewNumber(nextNumber);
+
+	/*float const time{GetFrameTime()};
+	int const diff{ static_cast<int>((m_targetNumber - m_currentNumber) * m_countingSpeed * time )};
+	SetNewNumber(m_currentNumber - diff);
+	*/
+}
+void CountingNumber::SetNewNumber(int number) {
+	m_currentNumber = number;
+	m_text->SetText(std::to_string(m_currentNumber));
+}
+
 CountingNumber::CountingNumber(Vector2 pos, Vector2 size, Alignment alignment, Vector2 resolution,
 	Alignment textAlignment, float textHeight, int startNumber) 
-	: UIElement(pos, size, alignment, resolution), m_currentNumber{ startNumber }, m_targetNumber{ startNumber }
+	: UIElement(pos, size, alignment, resolution),
+	m_startNumber{ startNumber }, m_currentNumber { startNumber }, m_targetNumber{ startNumber }
 {
 	m_text = std::make_shared<Text>(
 		pos,
@@ -28,10 +80,12 @@ bool CountingNumber::IsCounting() const {
 void CountingNumber::SetCallback(callback_ty callback) {
 	m_callback = callback;
 }
-void CountingNumber::CountTo(Type type, int target, float timeIsS) {
+void CountingNumber::CountTo(Type type, int target, double timeIsS) {
 	m_countingType = type;
 	m_targetNumber = target;
 	m_timeInS = timeIsS;
+	m_startNumber = m_currentNumber;
+	m_countingSpeed = { static_cast<float>((m_targetNumber - m_startNumber) / timeIsS )};
 }
 int CountingNumber::GetCurrentNumber() const {
 	return m_currentNumber;
@@ -41,7 +95,7 @@ int CountingNumber::GetTargetNumber() const {
 }
 
 void CountingNumber::CheckAndUpdate(Vector2 const& mousePosition, AppContext_ty_c appContext) {
-	// HandleCounting();
+	HandleCounting();
 	m_text->CheckAndUpdate(mousePosition, appContext);
 }
 void CountingNumber::Render(AppContext_ty_c appContext) {
--- TentakelsAttacking2/UI/Elements/Default/public/CountingNumber.h
+++ TentakelsAttacking2/UI/Elements/Default/public/CountingNumber.h
@@ -19,12 +19,16 @@ private:
 
 	bool m_isCounting{ false };
 	Type m_countingType{ LINEAR };
+	int m_startNumber;
 	int m_currentNumber;
 	int m_targetNumber;
-	float m_timeInS{ 0.0f };
 
-	using callback_ty = std::function<void(Type, int, float)>;
-	callback_ty m_callback{[](Type, int, float) ->void {} };
+	double m_timeInS{ 0.0f };
+	double m_startCountingTime{ GetTime() };
+	float m_countingSpeed{ 0.0f };
+
+	using callback_ty = std::function<void(Type, int, int, double)>;
+	callback_ty m_callback{[](Type, int, int, double) ->void {} };
 
 	void HandleCounting();
 	void HandleLinearCounting();
@@ -46,7 +50,7 @@ public:
 	/**
 	 * counts in a certain type to a other number in a provided time in s.
 	 */
-	void CountTo(Type type, int target, float timeIsS);
+	void CountTo(Type type, int target, double timeIsS);
 	/**
 	 * returns the current number.
 	 * differs from target number while counting.
