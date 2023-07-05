ac517005b7dfb2b5b03eb037d8aacecfda11f538
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-05 23:40:22 +0200

    merge fleet popup

--- TentakelsAttacking2/UI/Scene/MainScenes/private/UpdateEvaluation.cpp
+++ TentakelsAttacking2/UI/Scene/MainScenes/private/UpdateEvaluation.cpp
@@ -41,16 +41,94 @@ void UpdateEvaluationScene::Initialize(SendUpdateEvaluation const* event) const 
 		Print("------------------------------------------------------", PrintType::DEBUG);
 	}
 	Print("------------------------------------------------------", PrintType::DEBUG);
-
-	ShowMessagePopUpEvent sendEvent{
-		"Update",
-		"Logic is updating\ndisplayed in console",
-		[]() {
-				SwitchSceneEvent sendEvent{ SceneType::MAIN };
-				AppContext::GetInstance().eventManager.InvokeEvent(sendEvent);
-			}
+}
+void UpdateEvaluationScene::DisplayMergeResult() {
+	AppContext_ty_c appContext{ AppContext::GetInstance() };
+	auto const data {m_mergeResults.at(m_currentIndex)};
+	auto const playerName{
+		appContext.playerCollection.GetPlayerByID(
+			data.GetPlayer()->GetID()
+		).GetName()
 	};
-	appContext.eventManager.InvokeEvent(sendEvent);
+	
+	std::string spaceOpjectText;
+	     if (data.GetDestination()->IsFleet())       { spaceOpjectText = appContext.languageManager.Text("helper_fleet");        }
+	else if (data.GetDestination()->IsPlanet())      { spaceOpjectText = appContext.languageManager.Text("helper_planet");       }
+	else if (data.GetDestination()->IsTargetPoint()) { spaceOpjectText = appContext.languageManager.Text("helper_target_point"); }
+	else                                             { spaceOpjectText = "Invalid";                                              }
+
+	std::string const subText{
+		appContext.languageManager.Text(
+			"ui_popup_arriving_fleet_subtitle",
+			data.GetCount(),
+			playerName,
+			spaceOpjectText,
+			data.GetDestination()->GetID()
+		)
+	};
+
+	ShowMessagePopUpEvent const event{
+		appContext.languageManager.Text("ui_popup_arriving_fleet_title"),
+		subText,
+		[this]() {this->HandleNextPopup(); }
+	};
+	appContext.eventManager.InvokeEvent(event);
+}
+void UpdateEvaluationScene::DisplayFightResult() {
+	// implement fight result popup
+}
+void UpdateEvaluationScene::HandleNextPopup() {
+
+	auto const setNext{ [&]() {
+		m_currentIndex = 0;
+		int value = static_cast<int>(m_currentResultType);
+		++value;
+		m_currentResultType = static_cast<ResultType>(value);
+	} };
+
+	switch (m_currentResultType) {
+		case ResultType::MERGE:
+			if (m_currentIndex >= m_mergeResults.size()) {
+				setNext();
+				goto fight;
+			}
+			DisplayMergeResult();
+			break;
+		case ResultType::FIGHT:
+		fight:		
+			if (m_currentIndex >= m_fightResults.size()) {
+				setNext();
+				goto last;
+			}
+			DisplayFightResult();
+			break;
+		default:
+		case ResultType::LAST:
+		last:
+			AppContext_ty_c appContext{ AppContext::GetInstance() };
+			if (m_popupCount == 0) {
+				ShowMessagePopUpEvent const event{
+					appContext.languageManager.Text("ui_popup_no_evaluation_title"),
+					appContext.languageManager.Text("ui_popup_no_evaluation_subtitle"),
+					[this]() {this->HandleNextPopup(); }
+				};
+				appContext.eventManager.InvokeEvent(event);
+				break;
+			}
+			ShowMessagePopUpEvent const event{
+				appContext.languageManager.Text("ui_popup_end_of_evaluation_title"),
+				appContext.languageManager.Text("ui_popup_end_of_evaluation_subtitle"),
+				[]() {
+					SwitchSceneEvent const event{ SceneType::MAIN };
+					AppContext::GetInstance().eventManager.InvokeEvent(event);
+				}
+			};
+			appContext.eventManager.InvokeEvent(event);
+			break;
+	}
+
+	++m_popupCount;
+	++m_currentIndex;
 }
 
 UpdateEvaluationScene::UpdateEvaluationScene(Vector2 resolution)
@@ -66,6 +144,9 @@ UpdateEvaluationScene::~UpdateEvaluationScene() {
 
 void UpdateEvaluationScene::OnEvent(Event const& event) {
 	if (auto const* evEvent = dynamic_cast<SendUpdateEvaluation const*> (&event)) {
+		m_mergeResults = evEvent->GetMergeResults();
+		m_fightResults = evEvent->GetFightResults();
+		HandleNextPopup();
 		Initialize(evEvent);
 	}
 }
--- TentakelsAttacking2/UI/Scene/MainScenes/public/UpdateEvaluation.h
+++ TentakelsAttacking2/UI/Scene/MainScenes/public/UpdateEvaluation.h
@@ -11,8 +11,24 @@ class SendUpdateEvaluation;
 
 class UpdateEvaluationScene final : public Scene, public EventListener {
 private:
+	enum class ResultType {
+		MERGE,
+		FIGHT,
+		LAST,
+	};
+
+private:
+	std::vector<HMergeResult> m_mergeResults;
+	std::vector<HFightResult> m_fightResults;
+	ResultType m_currentResultType{ ResultType::MERGE };
+	int m_currentIndex{ 0 };
+	int m_popupCount{ 0 };
+
 	void Initialize(SendUpdateEvaluation const* event) const;
 
+	void DisplayMergeResult();
+	void DisplayFightResult();
+	void HandleNextPopup();
 
 public:
 	UpdateEvaluationScene(Vector2 resolution);
