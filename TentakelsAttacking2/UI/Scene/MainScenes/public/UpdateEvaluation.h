a2173b1b04ae7b9d32b5af06802626eea83fa2c4
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-06 20:08:21 +0200

    docu

--- TentakelsAttacking2/UI/Scene/MainScenes/public/UpdateEvaluation.h
+++ TentakelsAttacking2/UI/Scene/MainScenes/public/UpdateEvaluation.h
@@ -18,24 +18,51 @@ private:
 	};
 
 private:
-	std::vector<HMergeResult> m_mergeResults;
-	std::vector<HFightResult> m_fightResults;
-	ResultType m_currentResultType{ ResultType::MERGE };
-	int m_currentIndex{ 0 };
-	int m_popupCount{ 0 };
-	bool m_nextPopup{ false };
+	std::vector<HMergeResult> m_mergeResults; ///< contains the fleet merge results via event from the logic
+	std::vector<HFightResult> m_fightResults; ///< contains the fight results via event from the logic
+	ResultType m_currentResultType{ ResultType::MERGE }; ///< contains the current type for the correct popup 
+	int m_currentIndex{ 0 }; ///< contains the current index within the current type
+	int m_popupCount{ 0 }; ///< counts the overall ammount of popups displayed to know if it was 0
+	bool m_nextPopup{ false }; ///< contains if the next frame opens the next popup -> need so that the current popup has time to close itself
 
+	/**
+	 * prints the result to the console.
+	 * only for debugging.
+	 */
 	void TestPrint(SendUpdateEvaluation const* event) const;
 
+	/**
+	 * displays the next merge result popup.
+	 */
 	void DisplayMergeResult();
+	/**
+	 * displays the next fight result popup.
+	 */
 	void DisplayFightResult();
+	/**
+	 * handles witch popup is shown next.
+	 * calls the last popup when all popups are shown.
+	 */
 	void HandleNextPopup();
 
 public:
+	/**
+	 * adds the event listener to the event system.
+	 */
 	UpdateEvaluationScene(Vector2 resolution);
+	/**
+	 * removes the event listener from the event system.
+	 */
 	~UpdateEvaluationScene();
 
+	/**
+	 * calls the Scene to update.
+	 * calls the next popup.
+	 */
 	void CheckAndUpdate(Vector2 const& mousePosition, AppContext_ty_c appContext) override;
 
+	/**
+	 * receives the events.
+	 */
 	void OnEvent(Event const& event) override;
 };
