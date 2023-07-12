7836fca24959fee68c67752a5d0df7ad6b6ef92f
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-12 14:04:21 +0200

    rework game events scene

--- TentakelsAttacking2/UI/Scene/SupportScenes/private/GameEventSettings.cpp
+++ TentakelsAttacking2/UI/Scene/SupportScenes/private/GameEventSettings.cpp
@@ -7,11 +7,10 @@
 #include "Text.h"
 #include "CheckBox.h"
 #include "Line.h"
-#include "HGameEventTypes.hpp"
-#include "AppContext.h"
 #include "HRandom.h"
 #include <array>
-#include <iostream>
+#include <utility>
+#include <cassert>
 
 
 void GameEventSettings::Initialize(Vector2 resolution, unsigned int focusID) {
@@ -45,17 +44,9 @@ void GameEventSettings::Initialize(Vector2 resolution, unsigned int focusID) {
 	float const row     { 0.035f };
 	float const cbX     { 0.51f };
 	float const textX   { 0.49f };
-	std::array<std::string, 7> const text{
-		AppContext::GetInstance().languageManager.Text("ui_game_events_settings_event_global"         ),
-		AppContext::GetInstance().languageManager.Text("ui_game_events_settings_event_pirates"        ),
-		AppContext::GetInstance().languageManager.Text("ui_game_events_settings_event_revolts"        ),
-		AppContext::GetInstance().languageManager.Text("ui_game_events_settings_event_renegade_ships" ),
-		AppContext::GetInstance().languageManager.Text("ui_game_events_settings_event_black_hole"     ),
-		AppContext::GetInstance().languageManager.Text("ui_game_events_settings_event_supernova"      ),
-		AppContext::GetInstance().languageManager.Text("ui_game_events_settings_event_engine_problems")
-	};
 
-	for (unsigned int i = 0; i < text.size(); ++i) {
+
+	for (unsigned int i = 0; i < m_text.size(); ++i) {
 		auto displayText = std::make_shared<Text>(
 			GetElementPosition(textX, firstRow + row * i * 2),
 			GetElementSize(textX + 0.15f, row * 3),
@@ -63,7 +54,7 @@ void GameEventSettings::Initialize(Vector2 resolution, unsigned int focusID) {
 			resolution,
 			Alignment::MID_RIGHT,
 			row,
-			text.at(i)
+			m_text.at(i).second
 		);
 		// displayText->RenderRectangle(true);
 		m_elements.push_back(displayText);
@@ -76,111 +67,42 @@ void GameEventSettings::Initialize(Vector2 resolution, unsigned int focusID) {
 			resolution,
 			i
 		);
-		element->SetOnCheck([this](unsigned int ID, bool isChecked) {
-			this->SetChecked(ID, isChecked);
-			});
+		element->SetOnCheck([this](unsigned int index, bool isChecked) {
+			this->SetChecked(index, isChecked);
+		});
+		element->SetChecked(appContext.constants.gameEvents.IsFlag(m_text.at(i).first));
 		m_checkBoxes.push_back(element);
 		m_elements.push_back(element);
 	}
 }
 
-void GameEventSettings::SetChecked(unsigned int ID, bool isChecked) {
-	UpdateCheckGameEvent const event{
-		static_cast<HGameEventType>(ID),
-		isChecked
-	};
-	AppContext::GetInstance().eventManager.InvokeEvent(event);
-}
+void GameEventSettings::SetChecked(unsigned int index, bool isChecked) {
+	AppContext_ty appContext{ AppContext::GetInstance() };
+	appContext.constants.gameEvents.SetFlag(m_text.at(index).first, isChecked);
 
-void GameEventSettings::UpdateElements(UpdateCheckGameEventsUI const* event) {
+	assert (m_checkBoxes.size() == m_text.size());
 
-	auto const types{ event->GetTypes() };
-
-	for (auto const& [type, b] : *types) {
-		for (auto checkBox : m_checkBoxes) {
-			auto const cbType{ static_cast<HGameEventType>(checkBox->GetID()) };
-			if (cbType == type) {
-				checkBox->SetChecked(b);
-				continue;
-			}
-		}
-	}
-
-	SetGlobalCheckbox();
-}
-void GameEventSettings::SetGlobalCheckbox(){
-	CheckBox_ty globalCheckbox{ nullptr };
-	bool check{ true };
-	bool value{ false };
-
-	for (size_t i = 0; i < m_checkBoxes.size(); ++i) {
-		auto const cbTypeI{ static_cast<HGameEventType>(m_checkBoxes.at(i)->GetID()) };
-		if (cbTypeI == HGameEventType::GLOBAL) {
-			globalCheckbox = m_checkBoxes.at(i);
-			continue;
-		}
-		value = m_checkBoxes.at(i)->IsChecked();
-		for (size_t j = i; j < m_checkBoxes.size(); ++j) {
-			auto cbTypeJ{ static_cast<HGameEventType>(m_checkBoxes.at(i)->GetID()) };
-			if (cbTypeJ == HGameEventType::GLOBAL) {
-				continue;
-			}
-			if (m_checkBoxes.at(i)->IsChecked()
-				!= m_checkBoxes.at(j)->IsChecked()) {
-				check = false;
-				goto found;
-			}
-		}
-	}
-
-found:
-	if (check) {
-		globalCheckbox->SetChecked(value);
-	}
-	else {
-		globalCheckbox->SetChecked(false);
+	for (int i = 0; i < m_checkBoxes.size(); ++i) {
+		m_checkBoxes.at(i)->SetChecked(appContext.constants.gameEvents.IsFlag(m_text.at(i).first));
 	}
 }
 
 GameEventSettings::GameEventSettings(unsigned int focusID, Vector2 pos, Vector2 size,
 	Alignment alignment, Vector2 resolution)
 	: Scene{ pos, size, alignment, resolution } {
-	AppContext_ty appContext = AppContext::GetInstance();
-	appContext.eventManager.AddListener(this);
 
 	Initialize(resolution, focusID);
-
-	InitialCheckGameEventDataEvent const event;
-	appContext.eventManager.InvokeEvent(event);
-}
-GameEventSettings::~GameEventSettings() {
-	AppContext::GetInstance().eventManager.RemoveListener(this);
 }
 
-void GameEventSettings::OnEvent(Event const& event) {
-
-	if (auto const* UpdateEvent = dynamic_cast<UpdateCheckGameEventsUI const*>(&event)) {
-		UpdateElements(UpdateEvent);
-		return;
-	}
-}
 
 void GameEventSettings::SetRandom() {
 	Random& random{ Random::GetInstance() };
 	AppContext_ty_c appContext{ AppContext::GetInstance() };
 
 	for (auto& c : m_checkBoxes) {
-		if (c->GetID() == 0) { continue; } ///< id 0 is the gloabl check box
-
 		bool r = random.random(2) == 1;
 		if (c->IsChecked() != r) {
-			UpdateCheckGameEvent const event{
-				static_cast<HGameEventType>(c->GetID()),
-				r
-			};
-			appContext.eventManager.InvokeEvent(event);
+			c->SetChecked(r);
 		}
 	}
-
-	SetGlobalCheckbox();
 }
