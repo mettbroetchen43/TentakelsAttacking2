26043584efd2461fbf1d0c8b00acdbf1a16b8c2c
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-18 22:03:04 +0200

    update config version

--- TentakelsAttacking2/Constants/public/CGlobal.hpp
+++ TentakelsAttacking2/Constants/public/CGlobal.hpp
@@ -17,7 +17,7 @@ struct CGlobal final {
 
 	// versions
 	static inline std::string const languageVersion{"v0.1.5.0"}; ///< contains the language version string
-	static inline std::string const configVersion{ "v0.1.4.1" }; ///< contains the config version string
+	static inline std::string const configVersion{ "v0.1.5.0" }; ///< contains the config version string
 	// gameVersion will be extracted with regex in the GitHub CI. Don't chance the following line. see CMakeList.txt
 	static inline std::string const gameVersion{ "v0.1.4" }; ///< current gameVersion string
 	static inline std::string const copyRight{ "(c) Purpur Tentakel" }; ///< copy right string
